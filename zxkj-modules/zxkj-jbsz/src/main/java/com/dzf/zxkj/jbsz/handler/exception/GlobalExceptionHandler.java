814991c729cb37c62c218230558d74a8614d639c
高志新 <gzhx>
2019-09-04 15:47:14 +0800

    整理银行账户

--- zxkj-modules/zxkj-jbsz/src/main/java/com/dzf/zxkj/jbsz/handler/exception/GlobalExceptionHandler.java
+++ zxkj-modules/zxkj-jbsz/src/main/java/com/dzf/zxkj/jbsz/handler/exception/GlobalExceptionHandler.java
@@ -18,24 +18,24 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(BusinessException.class)
     @ResponseBody
     public ReturnData exceptionHandler(BusinessException e) {
-        return ReturnData.error().message(e.getMessage()).data(e);
+        return ReturnData.error().message(e.getMessage());
     }
 
     @ExceptionHandler(DZFWarpException.class)
     @ResponseBody
     public ReturnData exceptionHandler(DZFWarpException e) {
-        return ReturnData.error().message(e.getMessage()).data(e);
+        return ReturnData.error().message(e.getMessage());
     }
 
     @ExceptionHandler(RuntimeException.class)
     @ResponseBody
     public ReturnData exceptionHandler(RuntimeException e) {
-        return ReturnData.error().message(e.getMessage()).data(e);
+        return ReturnData.error().message(e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public ReturnData exceptionHandler(Exception e) {
-        return ReturnData.error(HttpStatus.INTERNAL_SERVER_ERROR.value()).message(e.getMessage()).data(e);
+        return ReturnData.error(HttpStatus.INTERNAL_SERVER_ERROR.value()).message(e.getMessage());
     }
 }
