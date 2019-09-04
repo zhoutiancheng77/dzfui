3e99edc3a56c6c03952ac78a992408bb3604677c
高志新 <gzhx>
2019-09-04 17:50:55 +0800

    整理银行账户

--- zxkj-modules/zxkj-jbsz/src/main/java/com/dzf/zxkj/jbsz/mapper/BankAccountMapper.java
+++ zxkj-modules/zxkj-jbsz/src/main/java/com/dzf/zxkj/jbsz/mapper/BankAccountMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.dzf.zxkj.jbsz.vo.BankAccountVO;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,9 +17,9 @@ import java.util.List;
 @Repository
 public interface BankAccountMapper extends BaseMapper<BankAccountVO> {
 
-    List<BankAccountVO> query(String pk_corp, String isnhsty);
+    List<BankAccountVO> query(@Param("pk_corp") String pk_corp, @Param("isnhsty") String isnhsty);
 
-    IPage<BankAccountVO> query(Page page, String pk_corp, String isnhsty);
+    IPage<BankAccountVO> query(Page page, @Param("pk_corp") String pk_corp, @Param("isnhsty") String isnhsty);
 
-    int existsInBankstatement(String pk_corp, String bankaccount);
+    int existsInBankstatement(@Param("pk_corp") String pk_corp, @Param("bankaccount") String bankaccount);
 }
