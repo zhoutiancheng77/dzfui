b4473389b0e95a4d0413ba8e496192723004f7ac
高志新 <gzhx>
2019-09-06 14:21:16 +0800

    swagger2

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.dzf.zxkj.platform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Auther: dandelion
+ * @Date: 2019-09-06
+ * @Description:
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.dzf.zxkj.platform.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("api文档")
+                .description("restfun 风格接口")
+                .version("1.0")
+                .build();
+    }
+}
