66b8125f0f8bb175ad96bcb0abe9520dca757dd7
高志新 <gzhx>
2019-09-24 17:30:41 +0800

    1

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/services/icset/IInventoryAccAliasService.java
@@ -0,0 +1,22 @@
+package com.dzf.zxkj.platform.services.icset;
+
+import com.dzf.zxkj.common.exception.DZFWarpException;
+import com.dzf.zxkj.platform.model.icset.InventoryAliasVO;
+
+public interface IInventoryAccAliasService {
+	
+	public InventoryAliasVO[] query(String pk_corp, String pk_inventory) throws DZFWarpException;
+
+	public InventoryAliasVO save(InventoryAliasVO vo1) throws DZFWarpException;
+
+	public void delete(String pk_icalias, String pk_corp) throws DZFWarpException;
+
+	public InventoryAliasVO[] insertAliasVOS(InventoryAliasVO[] vos, String pk_corp) throws DZFWarpException;
+
+	public void deleteByPks(String[] pk_icaliass, String pk_corp) throws DZFWarpException;
+
+	public void deleteByInvs(String[] pk_icaliass, String pk_corp) throws DZFWarpException;
+
+	public InventoryAliasVO[] updateAliasVOS(InventoryAliasVO[] vos, String pk_corp, String[] fields) throws DZFWarpException;
+
+}
