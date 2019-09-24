845d3fa10482f05dc52970c5d5de3a1be038b554
高志新 <gzhx>
2019-09-24 16:12:44 +0800

    1

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/services/bdset/ICptransLrService.java
@@ -0,0 +1,20 @@
+package com.dzf.zxkj.platform.services.bdset;
+
+import com.dzf.zxkj.common.exception.DZFWarpException;
+import com.dzf.zxkj.platform.model.bdset.YntCptranslrHVO;
+
+import java.util.List;
+
+public interface ICptransLrService {
+    // 保存
+    YntCptranslrHVO save(YntCptranslrHVO vo) throws DZFWarpException;
+
+    // 查询
+    List<YntCptranslrHVO> query(String pk_corp, boolean isgroup) throws DZFWarpException;
+
+    // 删除
+    void delete(YntCptranslrHVO vo) throws DZFWarpException;
+
+    YntCptranslrHVO queryById(String id) throws DZFWarpException;
+
+}
