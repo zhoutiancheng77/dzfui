c7f7f00d60c19add5087ddfb4b44f18a1130bca6
高志新 <gzhx>
2019-09-24 16:33:59 +0800

    1

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/services/bdset/IPersonalSetService.java
@@ -0,0 +1,17 @@
+package com.dzf.zxkj.platform.services.bdset;
+
+
+import com.dzf.zxkj.common.exception.DZFWarpException;
+import com.dzf.zxkj.platform.model.bdset.GxhszVO;
+
+/**
+ * 工资表设置
+ * @author zhangj
+ *
+ */
+public interface IPersonalSetService {
+
+//	public void save(GxhszVO vo, String pk_corp) throws DZFWarpException;
+
+	public GxhszVO query(String pk_corp) throws DZFWarpException;
+}
