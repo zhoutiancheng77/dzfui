855372ab699b67f4858de9709c0579f80729b0d2
高志新 <gzhx>
2019-09-24 16:17:17 +0800

    1

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/services/bdset/IHLService.java
@@ -0,0 +1,25 @@
+package com.dzf.zxkj.platform.services.bdset;
+
+import com.dzf.zxkj.common.exception.DZFWarpException;
+import com.dzf.zxkj.platform.model.bdset.ExrateVO;
+
+import java.util.List;
+
+public interface IHLService {
+
+    //保存
+    ExrateVO save(ExrateVO vo) throws DZFWarpException;
+
+    //更新
+    void update(ExrateVO vo) throws DZFWarpException;
+
+    //查询
+    List<ExrateVO> query(String pk_corp) throws DZFWarpException;
+
+    //查询
+    ExrateVO queryById(String id) throws DZFWarpException;
+
+    //删除
+    void delete(ExrateVO vo) throws DZFWarpException;
+
+}
