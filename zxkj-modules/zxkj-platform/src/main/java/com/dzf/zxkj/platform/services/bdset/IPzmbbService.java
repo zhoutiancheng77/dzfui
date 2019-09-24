19ea9c1077c86a1640d25d5e72299fe0cac379a8
高志新 <gzhx>
2019-09-24 16:35:49 +0800

    1

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/services/bdset/IPzmbbService.java
@@ -0,0 +1,25 @@
+package com.dzf.zxkj.platform.services.bdset;
+
+import com.dzf.zxkj.common.exception.BusinessException;
+import com.dzf.zxkj.platform.model.bdset.PzmbbVO;
+
+import java.util.List;
+
+public interface IPzmbbService {
+
+    //保存
+    PzmbbVO save(PzmbbVO vo) throws BusinessException;
+
+    //修改
+    void update(PzmbbVO vo);
+
+    //查询
+    List<PzmbbVO> query();
+
+    //查询
+    List<PzmbbVO> queryByPId(String PId);
+
+    //删除
+    void delete(PzmbbVO vo) throws BusinessException;
+
+}
