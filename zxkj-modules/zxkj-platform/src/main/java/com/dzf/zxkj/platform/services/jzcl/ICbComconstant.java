f62673d09698aa02b9feb3b84e5e203b794f16ee
高志新 <gzhx>
2019-09-24 20:36:48 +0800

    1

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/services/jzcl/ICbComconstant.java
@@ -0,0 +1,42 @@
+package com.dzf.zxkj.platform.services.jzcl;
+
+public interface ICbComconstant {
+
+	public String getFzcb2007(String pk_corp);
+
+	public String getZzfy2007();
+
+	public String getFzcb2013(String pk_corp);
+
+	public String getZzfy2013();
+
+	public String getJbcb_zjcl2007(String pk_corp);
+
+	public String getJbcb_zjrg2007(String pk_corp);
+
+	public String getJbcb_zzfy2007(String pk_corp);
+
+	public String getJbcb_zjcl2013(String pk_corp);
+
+	public String getJbcb_zjrg2013(String pk_corp);
+
+	public String getJbcb_zzfy2013(String pk_corp);
+	
+	public String  getJzcb_ycl2007(String pk_corp);
+
+	public String getJzcb_kcsp2007(String pk_corp);
+
+	public String getJzcb_ycl2013(String pk_corp);
+
+	public String getJzcb_kcsp2013(String pk_corp);
+
+	public String getKcsp_code();
+
+	public String getYcl_code();
+
+	public String getZg_code();
+
+	public String getFangan_2013();
+
+	public String getFangan_2007();
+}
