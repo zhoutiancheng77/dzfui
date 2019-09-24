65e9a0d7278f4fcf7e836b0e320fa6f9541ac14a
高志新 <gzhx>
2019-09-24 17:37:51 +0800

    1

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/services/icset/IInventoryAccSetService.java
@@ -0,0 +1,17 @@
+package com.dzf.zxkj.platform.services.icset;
+
+import com.dzf.zxkj.common.exception.DZFWarpException;
+import com.dzf.zxkj.platform.model.icset.InventorySetVO;
+import com.dzf.zxkj.platform.model.sys.CorpVO;
+
+public interface IInventoryAccSetService {
+
+    InventorySetVO query(String pk_corp) throws DZFWarpException;
+
+    InventorySetVO save(String userid, String pk_corp, InventorySetVO vo1, boolean ischeck) throws DZFWarpException;
+
+    InventorySetVO getDefaultValue(String userid, CorpVO cpvo) throws DZFWarpException;
+
+    String checkInventorySet(String userid, String pk_corp, InventorySetVO vo) throws DZFWarpException;
+
+}
