08733a2422d73f64491339b3f50ec57b8932315f
高志新 <gzhx>
2019-09-24 21:02:20 +0800

    1

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/services/icset/IInvAccSetService.java
@@ -0,0 +1,14 @@
+package com.dzf.zxkj.platform.services.icset;
+
+import com.dzf.zxkj.common.exception.DZFWarpException;
+import com.dzf.zxkj.platform.model.icset.InvAccSetVO;
+import com.dzf.zxkj.platform.model.sys.CorpVO;
+
+public interface IInvAccSetService {
+
+	InvAccSetVO query(String pk_corp) throws DZFWarpException;
+
+	InvAccSetVO save(InvAccSetVO vo1) throws DZFWarpException;
+	
+	InvAccSetVO saveGroupVO(CorpVO cpvo) throws DZFWarpException;
+}
