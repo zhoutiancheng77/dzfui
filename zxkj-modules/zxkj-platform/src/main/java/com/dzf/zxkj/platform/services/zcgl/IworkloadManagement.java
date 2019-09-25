89600c1fab0a8d5c2dc77c88b07cc78539feab14
高志新 <gzhx>
2019-09-25 17:06:15 +0800

    1

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/services/zcgl/IworkloadManagement.java
@@ -0,0 +1,21 @@
+package com.dzf.zxkj.platform.services.zcgl;
+
+import com.dzf.zxkj.common.exception.DZFWarpException;
+import com.dzf.zxkj.platform.model.zcgl.WorkloadManagementVO;
+import com.dzf.zxkj.platform.vo.sys.QueryParamVO;
+
+import java.util.List;
+
+public interface IworkloadManagement {
+	/**
+	 * 查询工作量法数据
+	 * @param paramvo
+	 * @return
+	 * @throws BusinessException
+	 */
+	public List<WorkloadManagementVO> queryWorkloadManagement(QueryParamVO paramvo) throws DZFWarpException;
+
+	List<WorkloadManagementVO> queryBypk_assetcard(QueryParamVO paramvo)throws DZFWarpException;
+
+	public void save(WorkloadManagementVO data)throws DZFWarpException;
+}
