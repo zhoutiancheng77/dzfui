5580c42dd7a01fb96d6eacc7fa0972c4b3645530
高志新 <gzhx>
2019-09-25 17:48:20 +0800

    1

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/services/report/ILrbQuarterlyReport.java
@@ -0,0 +1,39 @@
+package com.dzf.zxkj.platform.services.report;
+
+import com.dzf.zxkj.common.exception.DZFWarpException;
+import com.dzf.zxkj.common.lang.DZFDouble;
+import com.dzf.zxkj.platform.model.report.LrbquarterlyVO;
+import com.dzf.zxkj.platform.vo.sys.QueryParamVO;
+
+import java.util.Map;
+
+public interface ILrbQuarterlyReport {
+
+	/**
+	 * 利润表取数
+	 * @param period
+	 * @param pk_corp
+	 * @return
+	 * @throws BusinessException
+	 */
+	public LrbquarterlyVO[] getLRBquarterlyVOs(QueryParamVO paramVO) throws DZFWarpException;
+	
+	
+	/**
+	 * 从科目明细账取数据
+	 * @param paramVO
+	 * @return
+	 * @throws DZFWarpException
+	 */
+	public Map<String, LrbquarterlyVO[]> getLRBquarterlyVOs(QueryParamVO vo, Object[] objs) throws  DZFWarpException ;
+
+
+	/**
+	 * 每个月的利润表取数
+	 * @param paramVO
+	 * @return
+	 * @throws BusinessException
+	 */
+	public Map<String, DZFDouble> getYearLRBquarterlyVOs(String year, String pk_corp) throws  DZFWarpException ;
+	
+}
