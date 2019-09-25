766968561e3d75ad8e3d300ed8ba59452821b96d
高志新 <gzhx>
2019-09-25 11:18:34 +0800

    1

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/services/pzgl/impl/CreatePub.java
@@ -0,0 +1,31 @@
+package com.dzf.zxkj.platform.services.pzgl.impl;
+
+import com.dzf.zxkj.base.dao.SingleObjectBO;
+import com.dzf.zxkj.common.exception.BusinessException;
+import com.dzf.zxkj.platform.model.bdset.YntCpaccountVO;
+
+public class CreatePub {
+
+	private SingleObjectBO singleobjectbo;
+	
+	
+	public SingleObjectBO getSingleobjectbo() {
+		return singleobjectbo;
+	}
+
+
+//	public void setSingleobjectbo(SingleObjectBO singleobjectbo) {
+//		this.singleobjectbo = singleobjectbo;
+//	}
+
+
+	public CreatePub(SingleObjectBO singleobjectbo){
+		this.singleobjectbo=singleobjectbo;
+	}
+	
+	public YntCpaccountVO queryAccountVO(String pk_account) throws BusinessException {
+		YntCpaccountVO accountvo = (YntCpaccountVO)getSingleobjectbo().queryByPrimaryKey(YntCpaccountVO.class, pk_account);
+		return accountvo;
+	}
+	
+}
