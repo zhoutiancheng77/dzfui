ab61b7e9bc0aee2e86806fe1b43750528a8eef67
zhw <zhenghongwei@dazhangfang.com>
2019-10-21 13:57:38 +0800

    总账存货controller

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/glic/InvtorySetController.java
@@ -0,0 +1,16 @@
+package com.dzf.zxkj.platform.controller.glic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 存货设置
+ * @author zpm
+ *
+ */
+
+@RequestMapping("/gl/gl_icinvset")
+@Slf4j
+public class InvtorySetController {
+
+}
---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/glic/KcCbbController.java
@@ -0,0 +1,15 @@
+package com.dzf.zxkj.platform.controller.glic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 库存成本表
+ * @author wangzhn
+ *
+ */
+@RequestMapping("/gl/gl_rep_kcccbact")
+@Slf4j
+public class KcCbbController {
+
+}
