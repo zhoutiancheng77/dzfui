739c7e77bfb950527125fedf1bd5b7122f45057c
高志新 <gzhx>
2019-10-21 17:51:10 +0800

    去掉mvc注解

--- zxkj-modules/zxkj-report/src/main/java/com/dzf/zxkj/report/config/WebConfig.java
+++ zxkj-modules/zxkj-report/src/main/java/com/dzf/zxkj/report/config/WebConfig.java
@@ -7,14 +7,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.nio.charset.Charset;
 import java.util.List;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Bean
