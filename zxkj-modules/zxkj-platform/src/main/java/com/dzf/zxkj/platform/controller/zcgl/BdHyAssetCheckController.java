55372e527694e41a6e486323eabf09fd1d69b921
zhangj <zhangj@admin-PC>
2019-11-05 20:51:26 +0800

    总账对照

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/zcgl/BdHyAssetCheckController.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/zcgl/BdHyAssetCheckController.java
@@ -1,9 +1,27 @@
 package com.dzf.zxkj.platform.controller.zcgl;
 
 import com.dzf.zxkj.common.base.BaseController;
+import com.dzf.zxkj.common.constant.ISysConstants;
+import com.dzf.zxkj.common.entity.Grid;
+import com.dzf.zxkj.common.entity.Json;
+import com.dzf.zxkj.common.entity.ReturnData;
+import com.dzf.zxkj.common.enums.LogRecordEnum;
+import com.dzf.zxkj.common.lang.DZFDate;
+import com.dzf.zxkj.common.utils.IDefaultValue;
+import com.dzf.zxkj.common.utils.IGlobalConstants;
+import com.dzf.zxkj.common.utils.StringUtil;
+import com.dzf.zxkj.jackson.annotation.MultiRequestBody;
+import com.dzf.zxkj.platform.model.sys.BdAssetCategoryVO;
+import com.dzf.zxkj.platform.model.sys.BdTradeAssetCheckVO;
+import com.dzf.zxkj.platform.model.sys.CorpVO;
+import com.dzf.zxkj.platform.model.sys.UserVO;
+import com.dzf.zxkj.platform.service.zcgl.IHyAssetCheckService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 资产与总账对账[集团级 和公司级数据 同存在一个节点]
@@ -14,137 +32,95 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class BdHyAssetCheckController extends BaseController {
 
-//	@Autowired
-//	private IHyAssetCheckService services = null;
-//
-//	private String kmfaid;
-//
-//	public IHyAssetCheckService getServices() {
-//		return services;
-//	}
-//
-//	@Autowired
-//	public void setServices(IHyAssetCheckService services) {
-//		this.services = services;
-//	}
-//
-//	public void InitListInfo() {
-//		Grid grid = new Grid();
-//		try {
-//			List<BdTradeAssetCheckVO> list = null;
-//			CorpVO corpvo = getLoginCorpInfo();
-//			list = getServices().queryAssCheckVOs(corpvo.getPk_corp(), kmfaid);
-//			grid.setTotal(list == null ? 0 : (long) list.size());
-//			grid.setRows(list == null ? new ArrayList<BdTradeAssetCheckVO>() : list);
-//			grid.setSuccess(true);
-//		} catch (Exception e) {
-//			printErrorLog(grid, log, e, "初始化失败");
-//		}
-//
+	@Autowired
+	private IHyAssetCheckService sys_zczzdzbserv ;
+
+
+    @GetMapping("/query")
+	public ReturnData<Grid> query(@RequestParam("kmfaid") String kmfaid,@MultiRequestBody CorpVO corpVo) {
+		Grid grid = new Grid();
+		try {
+			List<BdTradeAssetCheckVO> list = null;
+			list = sys_zczzdzbserv.queryAssCheckVOs(corpVo.getPk_corp(), kmfaid);
+			grid.setTotal(list == null ? 0 : (long) list.size());
+			grid.setRows(list == null ? new ArrayList<BdTradeAssetCheckVO>() : list);
+			grid.setSuccess(true);
+		} catch (Exception e) {
+			printErrorLog(grid, e, "初始化失败");
+		}
+
 //		writeLogRecord(getLogType(), "公司资产与总账对照表操作", getSysIdent());
-//
+
 //		writeJson(grid);
-//	}
-//
-//	// 新增保存
-//	public void onSaveNew() {
-//		Json json = new Json();
-//		if (data != null) {
-//			data = getActionVO(BdTradeAssetCheckVO.class);
-//			data.setPk_corp(getLoginCorpInfo().getPk_corp());
-//			if (!getLoginCorpInfo().getPk_corp().equals(IGlobalConstants.currency_corp)) {
-//				data.setPk_trade_accountschema(getLoginCorpInfo().getCorptype());// 如果是公司的科目方案直接去公司的
-//			}
-//			data.setDoperatedate(new DZFDate());
-//			data.setCoperatorid(getLoginUserInfo().getCuserid());
-//			try {
-//				data = getServices().saveNew(data);
-//				json.setSuccess(true);
-//				json.setMsg("保存成功");
-//			} catch (Exception e) {
-//				printErrorLog(json, log, e, "保存失败");
-//			}
-//		} else {
-//			json.setSuccess(false);
-//			json.setMsg("保存失败");
-//		}
-//
+        return ReturnData.ok().data(grid);
+	}
+
+
+	// 新增保存
+    @PostMapping("/save")
+	public ReturnData onSaveNew(@MultiRequestBody BdTradeAssetCheckVO data, @MultiRequestBody CorpVO corpVO , @MultiRequestBody UserVO uservo) {
+		Json json = new Json();
+		if (data != null) {
+			data.setPk_corp(corpVO.getPk_corp());
+			if (!corpVO.getPk_corp().equals(IGlobalConstants.currency_corp)) {
+				data.setPk_trade_accountschema(corpVO.getCorptype());// 如果是公司的科目方案直接去公司的
+			}
+
+			try {
+			    if(StringUtil.isEmpty(data.getPrimaryKey())){
+                    data.setDoperatedate(new DZFDate());
+                    data.setCoperatorid(uservo.getCuserid());
+                    data = sys_zczzdzbserv.saveNew(data);
+                }else{
+                    sys_zczzdzbserv.update(data);
+                }
+				json.setSuccess(true);
+				json.setMsg("保存成功");
+			} catch (Exception e) {
+				printErrorLog(json, e, "保存失败");
+			}
+		} else {
+			json.setSuccess(false);
+			json.setMsg("保存失败");
+		}
+
 //		writeLogRecord(getLogType(),
 //				"新增资产与总账对照表数据", getSysIdent());
-//
-//		writeJson(json);
-//	}
-//
-//	// 修改保存
-//	public void onEditSave() {
-//		Json json = new Json();
-//		if (data != null) {
-//			try {
-//				if (getLoginCorpInfo().getPk_corp().equals(data.getPk_corp())) {
-//					data = getActionVO(BdTradeAssetCheckVO.class);
-//					// data.setPk_corp(getLoginCorpInfo().getPk_corp());
-//					if (!getLoginCorpInfo().getPk_corp().equals(IGlobalConstants.currency_corp)) {
-//						data.setPk_trade_accountschema(getLoginCorpInfo().getCorptype());// 如果是公司的科目方案直接去公司的
-//					}
-//					getServices().update(data);
-//					json.setSuccess(true);
-//					json.setMsg("保存成功");
-//				} else {
-//					json.setSuccess(false);
-//					json.setMsg("对不起，您无操作权限！");
-//				}
-//			} catch (Exception e) {
-//				printErrorLog(json, log, e, "保存失败");
-//			}
-//		} else {
-//			json.setSuccess(false);
-//			json.setMsg("保存失败");
-//		}
-//
-//		//记录日志
-//		writeLogRecord(getLogType(), "修改资产与总账对照表数据", getSysIdent());
-//
-//		writeJson(json);
-//	}
-//
-//	// 删除记录
-//	public void onDelete() {
-//		Json json = new Json();
-//		if (data != null) {
-//			try {
-//				if (getLoginCorpInfo().getPk_corp().equals(data.getPk_corp())) {
-//					data = getActionVO(BdTradeAssetCheckVO.class);
-//					data.setPk_corp(getLoginCorpInfo().getPk_corp());
-//					getServices().deleteInfovo(data);
-//					json.setSuccess(true);
-//					json.setMsg("删除成功！");
-//				} else {
-//					json.setSuccess(false);
-//					json.setMsg("对不起，您无操作权限！");
-//				}
-//			} catch (Exception e) {
-//				printErrorLog(json, log, e, "删除失败");
-//			}
-//		} else {
-//			json.setSuccess(false);
-//			json.setMsg("删除失败!");
-//		}
-//
-//		//记录日志
+
+        return ReturnData.ok().data(json);
+	}
+
+
+	// 删除记录
+    @PostMapping("/delete")
+	public ReturnData<Json> onDelete(@MultiRequestBody BdTradeAssetCheckVO data, @MultiRequestBody CorpVO corpVO ) {
+		Json json = new Json();
+		if (data != null) {
+			try {
+				if (corpVO.getPk_corp().equals(data.getPk_corp())) {
+					data.setPk_corp(corpVO.getPk_corp());
+                    sys_zczzdzbserv.deleteInfovo(data);
+					json.setSuccess(true);
+					json.setMsg("删除成功！");
+				} else {
+					json.setSuccess(false);
+					json.setMsg("对不起，您无操作权限！");
+				}
+			} catch (Exception e) {
+				printErrorLog(json, e, "删除失败");
+			}
+		} else {
+			json.setSuccess(false);
+			json.setMsg("删除失败!");
+		}
+
+		//记录日志
 //		writeLogRecord(getLogType(),  "删除资产与总账对照表数据",getSysIdent());
-//
-//		writeJson(json);
-//	}
-//
-//	public String getKmfaid() {
-//		return kmfaid;
-//	}
-//
-//	public void setKmfaid(String kmfaid) {
-//		this.kmfaid = kmfaid;
-//	}
-//
-//
+		return ReturnData.ok().data(json);
+	}
+
+
+
 //	private Integer getLogType(){
 //		Integer logtype = LogRecordEnum.OPE_JITUAN_PZMB.getValue();
 //        if (!IDefaultValue.DefaultGroup.equals(getLogincorppk())) {
