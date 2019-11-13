54d2617922e2f82861d8342edf912d8a08ecf8dc
zhw <zhenghongwei@dazhangfang.com>
2019-11-13 15:08:27 +0800

    存货别名

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/glic/InvtoryAliasController.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/glic/InvtoryAliasController.java
@@ -1,8 +1,12 @@
 package com.dzf.zxkj.platform.controller.glic;
 
+import com.dzf.zxkj.base.exception.BusinessException;
+import com.dzf.zxkj.base.utils.DZFStringUtil;
+import com.dzf.zxkj.base.utils.DZFValueCheck;
 import com.dzf.zxkj.common.entity.Grid;
 import com.dzf.zxkj.common.entity.Json;
 import com.dzf.zxkj.common.entity.ReturnData;
+import com.dzf.zxkj.common.query.QueryParamVO;
 import com.dzf.zxkj.common.utils.StringUtil;
 import com.dzf.zxkj.jackson.utils.JsonUtils;
 import com.dzf.zxkj.platform.model.glic.InventoryAliasVO;
@@ -12,7 +16,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -28,23 +31,44 @@ public class InvtoryAliasController {
     private IInventoryAccAliasService gl_ic_invtoryaliasserv = null;
 
     @GetMapping("/query")
-    public ReturnData query(@RequestParam("pk_inventory") String pk_inventory){
+    public ReturnData query(@RequestParam Map<String, String> param){
         Grid grid = new Grid();
+        String   pk_inventory= param.get("pk_inventory");
         InventoryAliasVO[] vos = gl_ic_invtoryaliasserv.query(SystemUtil.getLoginCorpId(),pk_inventory);
-        if(vos != null && vos.length>0){
-            grid.setRows(new ArrayList<InventoryAliasVO>(Arrays.asList(vos)));
-            grid.setTotal(Long.valueOf(vos.length));
-            grid.setMsg("查询成功");
-        }else{
-            grid.setTotal(0l);
-            grid.setMsg("查询数据为空");
+
+        String isfenye = param.get("isfenye");
+        QueryParamVO queryParamvo = JsonUtils.convertValue(param, QueryParamVO.class);
+        if("Y".equals(isfenye)) {
+            int page = queryParamvo.getPage();
+            int rows = queryParamvo.getRows();
+            if (vos != null && vos.length > 0) {
+                grid.setTotal((long) vos.length );
+                InventoryAliasVO[] PzglPagevos = getPagedZZVOs(vos, page, rows);
+                grid.setRows(Arrays.asList(PzglPagevos));
+            }
+        } else {
+            if (vos != null && vos.length  > 0) {
+                grid.setTotal((long)vos.length );
+                grid.setRows(vos);
+            }
         }
         grid.setSuccess(true);
         return ReturnData.ok().data(grid);
     }
 
+    // 将查询后的结果分页
+    private InventoryAliasVO[] getPagedZZVOs(InventoryAliasVO[] PzglPagevos, int page, int rows) {
+        int beginIndex = rows * (page - 1);
+        int endIndex = rows * page;
+        if (endIndex >= PzglPagevos.length) {// 防止endIndex数组越界
+            endIndex = PzglPagevos.length;
+        }
+        PzglPagevos = Arrays.copyOfRange(PzglPagevos, beginIndex, endIndex);
+        return PzglPagevos;
+    }
+
     @PostMapping("/save")
-    public ReturnData save(@RequestParam Map<String, String> param){
+    public ReturnData save(@RequestBody Map<String, String> param){
         Json json = new Json();
         String operateType = StringUtil.nequals(param.get("type"), "add")?"修改":"新增";
         String cateName = param.get("cate_name");
@@ -58,13 +82,18 @@ public class InvtoryAliasController {
         return ReturnData.ok().data(json);
     }
 
-    @GetMapping("/onDelete")
-    public ReturnData onDelete(@RequestParam Map<String, String> param){
+    @PostMapping("/onDelete")
+    public ReturnData onDelete(@RequestBody Map<String, String> param){
         Json json = new Json();
-        String pk_alias = param.get("pk_alias");
+        String pk_alias = param.get("pk_aliass");
         String cateName = param.get("cate_name");
         String aliasname =param.get("aliasname");
-        gl_ic_invtoryaliasserv.delete(pk_alias, SystemUtil.getLoginCorpId());
+
+        String[] pkss = DZFStringUtil.getString2Array(pk_alias, ",");
+        if (DZFValueCheck.isEmpty(pkss)){
+            throw new BusinessException("数据为空,删除失败!");
+        }
+        gl_ic_invtoryaliasserv.deleteByPks(pkss, SystemUtil.getLoginCorpId());
         json.setMsg("删除成功");
         json.setSuccess(true);
 //            writeLogRecord(LogRecordEnum.OPE_KJ_BDSET.getValue(), "存货档案_删除存货'"+cateName+"'别名：别名："+aliasname,ISysConstants.SYS_2);
