50980e594351ffc79f14161501e345c71978d9fd
zhangpuming <zhangpuming@dazhangfang.com>
2019-11-15 17:58:59 +0800

    zpm commit;

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/jzcl/QmclController.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/jzcl/QmclController.java
@@ -8,6 +8,7 @@ import com.dzf.zxkj.common.lang.DZFDate;
 import com.dzf.zxkj.common.query.QueryParamVO;
 import com.dzf.zxkj.jackson.annotation.MultiRequestBody;
 import com.dzf.zxkj.platform.model.jzcl.QmclVO;
+import com.dzf.zxkj.platform.model.sys.UserVO;
 import com.dzf.zxkj.platform.service.jzcl.IQmclService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @Author: zpm
@@ -57,4 +58,255 @@ public class QmclController {
         return ReturnData.ok().data(grid);
     }
 
+    @PostMapping("/onjtzj")
+    public ReturnData<Grid> onjtzj(@MultiRequestBody("qmvos")  QmclVO[] qmvos,@MultiRequestBody UserVO userVO) {
+        Grid grid = new Grid();
+        try {
+            Map<String, List<QmclVO>> qmclmap = new HashMap<String, List<QmclVO>>();
+            for (QmclVO votemp : qmvos) {
+                String pk_corp = votemp.getPk_corp();
+                if (qmclmap.containsKey(pk_corp)) {
+                    qmclmap.get(pk_corp).add(votemp);
+                } else {
+                    List<QmclVO> listtemp = new ArrayList<QmclVO>();
+                    listtemp.add(votemp);
+                    qmclmap.put(pk_corp, listtemp);
+                }
+            }
+            StringBuffer tips = new StringBuffer();
+            List<QmclVO> resqmcl = new ArrayList<QmclVO>();
+            // 先按照公司
+            for (String str : qmclmap.keySet()) {
+                List<QmclVO> listtemp = qmclmap.get(str);
+                QmclVO[] qmclvos = sortQmclByPeriod(listtemp, "asc");
+
+                for (QmclVO votemp : qmclvos) {
+                    try {
+                        gl_qmclserv.checkTemporaryIsExist(votemp.getPk_corp(), votemp.getPeriod(),"不能计提折旧!");
+                        votemp.setCoperatorid(userVO.getCuserid());
+                        QmclVO resvos = gl_qmclserv.updateJiTiZheJiu(votemp,userVO.getCuserid());
+                        resqmcl.add(resvos);
+                    } catch (Exception e) {
+                        if (e.getMessage().indexOf("null") >= 0) {
+                            tips.append("计提折旧失败:" + "<br>");
+                        } else {
+                            tips.append(e instanceof BusinessException ? e.getMessage() : "计提折旧失败<br>");
+                        }
+                        resqmcl.add(votemp);
+                        log.error("计提折旧失败:",e);
+                    }
+                }
+            }
+            if (tips.toString().length() > 0) {
+                grid.setSuccess(false);
+                grid.setMsg(tips.toString());
+            } else {
+                grid.setMsg("计提折旧成功！");
+                grid.setSuccess(true);
+            }
+            grid.setTotal((long) 1);
+            grid.setRows(resqmcl);
+        } catch (Exception e) {
+            log.error("错误",e);
+            grid.setSuccess(false);
+            grid.setRows(new ArrayList<QmclVO>());
+            grid.setMsg(e instanceof BusinessException ? e.getMessage() : "计提折旧失败！");
+        }
+        return ReturnData.ok().data(grid);
+    }
+
+    @PostMapping("/canceljtzj")
+    public ReturnData<Grid> canceljtzj(@MultiRequestBody("qmvos")  QmclVO[] qmvos) {
+        Grid grid = new Grid();
+        try {
+            // 重复调用接口，公司+月份
+            Map<String, List<QmclVO>> qmclmap = new HashMap<String, List<QmclVO>>();
+            for (int i = qmvos.length - 1; i >= 0; i--) {
+                QmclVO votemp = qmvos[i];
+                String pk_corp = votemp.getPk_corp();
+                if (qmclmap.containsKey(pk_corp)) {
+                    qmclmap.get(pk_corp).add(votemp);
+                } else {
+                    List<QmclVO> listtemp = new ArrayList<QmclVO>();
+                    listtemp.add(votemp);
+                    qmclmap.put(pk_corp, listtemp);
+                }
+            }
+            StringBuffer tips = new StringBuffer();
+            List<QmclVO> resqmcl = new ArrayList<QmclVO>();
+            // 先按照公司
+            for (String str : qmclmap.keySet()) {
+                List<QmclVO> listtemp = qmclmap.get(str);
+                QmclVO[] qmclvos = sortQmclByPeriod(listtemp, "desc");
+                for (QmclVO votemp : qmclvos) {
+                    try {
+                        QmclVO resvos = gl_qmclserv.updateFanJiTiZheJiu(votemp);
+                        resqmcl.add(resvos);
+                    } catch (Exception e) {
+                        tips.append(e instanceof BusinessException ? e.getMessage() :"反计提折旧失败！<br/>");
+                        resqmcl.add(votemp);
+                        log.error("错误",e);
+                    }
+                }
+            }
+            if (tips.toString().length() > 0) {
+                grid.setSuccess(false);
+                grid.setMsg(tips.toString());
+            } else {
+                grid.setSuccess(true);
+                grid.setMsg("反计提折旧成功！");
+            }
+            grid.setTotal((long) resqmcl.size());
+            grid.setRows(resqmcl);
+        } catch (Exception e) {
+            log.error("错误",e);
+            grid.setRows(new ArrayList<QmclVO>());
+            grid.setSuccess(false);
+            grid.setMsg(e instanceof BusinessException ? e.getMessage() : "反计提折旧失败！");
+        }
+        return ReturnData.ok().data(grid);
+    }
+
+
+    @PostMapping("/onzzsjz")
+    public ReturnData<Grid> onzzsjz(@MultiRequestBody("qmvos")  QmclVO[] qmvos,@MultiRequestBody UserVO userVO) {
+        Grid grid = new Grid();
+        try {
+            Map<String, List<QmclVO>> qmclmap = new HashMap<String, List<QmclVO>>();
+            for (QmclVO votemp : qmvos) {
+                String pk_corp = votemp.getPk_corp();
+                if (qmclmap.containsKey(pk_corp)) {
+                    qmclmap.get(pk_corp).add(votemp);
+                } else {
+                    List<QmclVO> listtemp = new ArrayList<QmclVO>();
+                    listtemp.add(votemp);
+                    qmclmap.put(pk_corp, listtemp);
+                }
+            }
+            StringBuffer tips = new StringBuffer();
+            List<QmclVO> resqmcl = new ArrayList<QmclVO>();
+            // 先按照公司
+            for (String str : qmclmap.keySet()) {
+                List<QmclVO> listtemp = qmclmap.get(str);
+                QmclVO[] qmclvos = sortQmclByPeriod(listtemp, "asc");
+                for (QmclVO votemp : qmclvos) {
+                    try {
+                        gl_qmclserv.checkTemporaryIsExist(votemp.getPk_corp(), votemp.getPeriod(), "不能增值税结转!");
+                        votemp.setCoperatorid(userVO.getCuserid());
+                        QmclVO resvos = gl_qmclserv.onzzsjz(userVO.getCuserid(), votemp);
+                        resqmcl.add(resvos);
+                    }  catch (Exception e) {
+                        tips.append(e instanceof BusinessException ? e.getMessage() :"增值税结转失败<br/>");
+                        resqmcl.add(votemp);
+                        log.error("错误", e);
+                    }
+                }
+            }
+            if (tips.toString().length() > 0) {
+                grid.setMsg(tips.toString());
+                grid.setSuccess(false);
+            } else {
+                grid.setMsg("增值税结转成功！");
+                grid.setSuccess(true);
+            }
+            grid.setTotal((long) resqmcl.size());
+            grid.setRows(resqmcl);
+        } catch (Exception e) {
+            log.error("错误",e);
+            grid.setSuccess(false);
+            grid.setRows(new ArrayList<QmclVO>());
+            grid.setMsg(e instanceof BusinessException ? e.getMessage() : "增值税结转失败！");
+        }
+        return ReturnData.ok().data(grid);
+    }
+
+
+    @PostMapping("/cancelzzsjz")
+    public ReturnData<Grid> cancelzzsjz(@MultiRequestBody("qmvos")  QmclVO[] qmvos) {
+        Grid grid = new Grid();
+        try {
+            // 重复调用接口，公司+月份
+            Map<String, List<QmclVO>> qmclmap = new HashMap<String, List<QmclVO>>();
+            for (int i = qmvos.length - 1; i >= 0; i--) {
+                QmclVO votemp = qmvos[i];
+                String pk_corp = votemp.getPk_corp();
+                if (qmclmap.containsKey(pk_corp)) {
+                    qmclmap.get(pk_corp).add(votemp);
+                } else {
+                    List<QmclVO> listtemp = new ArrayList<QmclVO>();
+                    listtemp.add(votemp);
+                    qmclmap.put(pk_corp, listtemp);
+                }
+            }
+            StringBuffer tips = new StringBuffer();
+            List<QmclVO> resqmcl = new ArrayList<QmclVO>();
+            // 先按照公司
+            for (String str : qmclmap.keySet()) {
+                List<QmclVO> listtemp = qmclmap.get(str);
+                QmclVO[] qmclvos = sortQmclByPeriod(listtemp, "desc");
+                for (QmclVO votemp : qmclvos) {
+                    try {
+                        QmclVO resvos = gl_qmclserv.onfzzsjz(votemp);
+                        resqmcl.add(resvos);
+                    }  catch (Exception e) {
+                        tips.append(e instanceof BusinessException ? e.getMessage() : "反增值税结转失败<br/>");
+                        resqmcl.add(votemp);
+                        log.error("错误", e);
+                    }
+                }
+            }
+            if (tips.toString().length() > 0) {
+                grid.setMsg(tips.toString());
+                grid.setSuccess(false);
+            } else {
+                grid.setMsg("反增值税结转成功！");
+                grid.setSuccess(true);
+            }
+            grid.setTotal((long) resqmcl.size());
+            grid.setRows(resqmcl);
+        } catch (Exception e) {
+            log.error("错误",e);
+            grid.setRows(new ArrayList<QmclVO>());
+            grid.setSuccess(false);
+            grid.setMsg(e instanceof BusinessException ? e.getMessage() : "反增值税结转失败！");
+        }
+        return ReturnData.ok().data(grid);
+    }
+
+
+    private QmclVO[] sortQmclByPeriod(List<QmclVO> listtemp, final String ordervalue) {
+        QmclVO[] qmclvos = listtemp.toArray(new QmclVO[0]);
+        // 先对集合排序
+        java.util.Arrays.sort(qmclvos, new Comparator<QmclVO>() {
+            public int compare(QmclVO o1, QmclVO o2) {
+                int i = 0;
+                if ("desc".equals(ordervalue)) {
+                    if (o1.getPeriod().compareTo(o2.getPeriod()) > 0) {
+                        i = -1;
+                    } else if (o1.getPeriod().compareTo(o2.getPeriod()) == 0) {
+                        i = 0;
+                    } else {
+                        i = 1;
+                    }
+                } else {
+                    i = o1.getPeriod().compareTo(o2.getPeriod());
+                }
+                return i;
+            }
+        });
+
+        return qmclvos;
+    }
+
+    private String getLogMsg(String ope, QmclVO[] qmclvos, String ident) {
+
+        StringBuffer value = new StringBuffer();
+        if (ident.equals("asc")) {
+            value.append(ope + ":" + qmclvos[0].getPeriod() + "~" + qmclvos[qmclvos.length - 1].getPeriod());
+        } else {
+            value.append(ope + ":" + qmclvos[qmclvos.length - 1].getPeriod() + "~" + qmclvos[0].getPeriod());
+        }
+
+        return value.toString();
+    }
 }
