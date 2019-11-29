e64049cb2c0511b727cb2e05a04d28c0bce3d26f
高志新 <gzhx>
2019-11-29 11:03:24 +0800

    打包处理

--- zxkj-gateway/src/main/java/com/dzf/zxkj/gateway/config/GatewayConfig.java
+++ zxkj-gateway/src/main/java/com/dzf/zxkj/gateway/config/GatewayConfig.java
@@ -2,7 +2,12 @@ package com.dzf.zxkj.gateway.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 import java.util.List;
 
@@ -16,4 +21,17 @@ public class GatewayConfig {
     private List<String> ignoreUrl;
 
     private byte[] userPubKey;
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
 }
