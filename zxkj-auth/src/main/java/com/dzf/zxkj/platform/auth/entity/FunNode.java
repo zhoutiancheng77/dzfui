2125805e7210ab7238fc629585f3afce72e7f5d9
高志新 <gzhx>
2019-12-04 20:43:22 +0800

    菜单控制

--- zxkj-auth/src/main/java/com/dzf/zxkj/platform/auth/entity/FunNode.java
+++ zxkj-auth/src/main/java/com/dzf/zxkj/platform/auth/entity/FunNode.java
@@ -3,6 +3,7 @@ package com.dzf.zxkj.platform.auth.entity;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @Data
@@ -10,9 +11,21 @@ import lombok.Data;
 public class FunNode {
     @TableField("module")
     private String module;
+    private String code;
+    @TableField("router")
+    private String router;
     @TableId
     private String pk_funnode;
+    @TableField("fun_name")
+    private String name;
     @TableField("nodeurl")
     private String nodeurl;
+    @JsonIgnore
     private String dr;
+    @TableField("pk_parent")
+    private String pk_parent;
+    @TableField("file_dir")
+    private String component;
+    @TableField("show_order")
+    private Integer orderNum;
 }
