733359341d43102c7699f83a07ac3fbbc761c8fb
yinyixuan <329327140@qq.com>
2019-12-18 09:27:58 +0800

    qidongredissuo

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/ZxkjPlatformApplication.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/ZxkjPlatformApplication.java
@@ -1,5 +1,6 @@
 package com.dzf.zxkj.platform;
 
+import com.dzf.cloud.redis.EnableDzfRedis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -13,6 +14,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(basePackages= {"com.dzf.zxkj.platform","com.dzf.zxkj.operate.log"})
+@EnableDzfRedis
 public class ZxkjPlatformApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZxkjPlatformApplication.class, args);
