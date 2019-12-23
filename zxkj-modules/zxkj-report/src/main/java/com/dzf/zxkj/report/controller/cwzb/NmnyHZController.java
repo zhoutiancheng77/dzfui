77114c83f152253cde868d0a7bcc813150ed731f
wangzhaoning <wangzhaoning@dazhangfang.com>
2019-12-23 15:13:13 +0800

    1

--- zxkj-modules/zxkj-report/src/main/java/com/dzf/zxkj/report/controller/cwzb/NmnyHZController.java
+++ zxkj-modules/zxkj-report/src/main/java/com/dzf/zxkj/report/controller/cwzb/NmnyHZController.java
@@ -2,32 +2,43 @@ package com.dzf.zxkj.report.controller.cwzb;
 
 import com.dzf.zxkj.base.dao.SingleObjectBO;
 import com.dzf.zxkj.base.exception.BusinessException;
-import com.dzf.zxkj.common.query.QueryCondictionVO;
 import com.dzf.zxkj.common.constant.IcCostStyle;
 import com.dzf.zxkj.common.entity.Grid;
 import com.dzf.zxkj.common.entity.ReturnData;
 import com.dzf.zxkj.common.lang.DZFBoolean;
 import com.dzf.zxkj.common.lang.DZFDate;
+import com.dzf.zxkj.common.model.ColumnCellAttr;
+import com.dzf.zxkj.common.model.SuperVO;
+import com.dzf.zxkj.common.query.PrintParamVO;
+import com.dzf.zxkj.common.query.QueryCondictionVO;
 import com.dzf.zxkj.common.utils.DateUtils;
+import com.dzf.zxkj.common.utils.StringUtil;
+import com.dzf.zxkj.excel.util.Excelexport2003;
+import com.dzf.zxkj.jackson.annotation.MultiRequestBody;
 import com.dzf.zxkj.jackson.utils.JsonUtils;
+import com.dzf.zxkj.pdf.PrintReporUtil;
 import com.dzf.zxkj.platform.model.bdset.YntCpaccountVO;
 import com.dzf.zxkj.platform.model.report.NumMnyGlVO;
 import com.dzf.zxkj.platform.model.sys.CorpVO;
+import com.dzf.zxkj.platform.model.sys.UserVO;
 import com.dzf.zxkj.platform.service.IZxkjPlatformService;
+import com.dzf.zxkj.report.controller.ReportBaseController;
+import com.dzf.zxkj.report.excel.cwzb.NmnyHzExcelField;
 import com.dzf.zxkj.report.service.cwzb.INummnyReport;
 import com.dzf.zxkj.report.utils.ReportUtil;
 import com.dzf.zxkj.report.utils.SystemUtil;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.util.*;
 
 /**
  * 数量金额总账
@@ -35,7 +46,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("gl_rep_sljezzact")
 @Slf4j
-public class NmnyHZController {
+public class NmnyHZController extends ReportBaseController {
 
     @Autowired
     private SingleObjectBO singleObjectBO;
@@ -127,5 +138,198 @@ public class NmnyHZController {
 
         }
     }
+
+    @PostMapping("print/pdf")
+    public void print(@MultiRequestBody PrintParamVO printParamVO,
+                      @MultiRequestBody UserVO userVO, @MultiRequestBody CorpVO corpVO, HttpServletResponse response){
+        try {
+            PrintReporUtil printReporUtil = new PrintReporUtil(zxkjPlatformService, corpVO, userVO, response);
+            Map<String, String> params = printReporUtil.getPrintMap(printParamVO);
+            String strlist = params.get("list");
+            String type = params.get("type");
+            String pageOrt = params.get("pageOrt");
+            String left = params.get("left");
+            String top = params.get("top");
+            String printdate = params.get("printdate");
+            String font = params.get("font");
+            String pageNum = params.get("pageNum");
+            String showbnljjf = printParamVO.getShowjf();
+            String showbnljdf = printParamVO.getShowdf();
+            Map<String,String> pmap=new HashMap<String,String>();//声明一个map用来存前台传来的设置参数
+            pmap.put("type",type);
+            pmap.put("pageOrt",pageOrt);
+            pmap.put("left",left);
+            pmap.put("top",top);
+            pmap.put("printdate",printdate);
+            pmap.put("font",font);
+            pmap.put("pageNum",pageNum);
+            if(strlist==null){
+                return;
+            }
+            if(pageOrt.equals("Y")){
+                printReporUtil.setIscross(DZFBoolean.TRUE);//是否横向
+            }
+            else{
+                printReporUtil.setIscross(DZFBoolean.FALSE);//是否横向
+            }
+//            JSONArray array = (JSONArray) JSON.parseArray(strlist);
+//            Map<String,String> bodymapping=FieldMapping.getFieldMapping(new ZcZzVO());
+//            NumMnyGlVO[] bodyvos =DzfTypeUtils.cast(array,bodymapping, NumMnyGlVO[].class, JSONConvtoJAVA.getParserConfig());
+            NumMnyGlVO[] bodyvos = JsonUtils.deserialize(strlist, NumMnyGlVO[].class);
+            Map<String, String> tmap = new HashMap<String, String>();// 声明一个map用来存前台传来的设置参数
+            tmap.put("公司", bodyvos[0].getGs());
+            tmap.put("期间", bodyvos[0].getTitlePeriod());
+            String corp = corpVO.getPk_corp();
+            CorpVO corpvo = (CorpVO)singleObjectBO.queryByPrimaryKey(CorpVO.class, corp);
+
+            printReporUtil.setLineheight(22f);
+            setDefaultValue(bodyvos, corp);//为后续设置精度赋值
+
+            String[] columnames = new String[]{"科目编码","科目名称","计量单位","方向","期初余额","本期借方","本期贷方","本年累计借方","本年累计贷方","期末余额","数量","单价","金额","数量","金额","数量","金额","数量","金额","数量","金额","数量","单价","金额"};
+            String[] columnkeys = new String[]{"kmbm","kmmc","dw","dir","qcnum","qcprice","qcmny","bqjfnum","bqjfmny","bqdfnum","bqdfmny","bnjfnum","bnjfmny","bndfnum","bndfmny","qmnum","qmprice","qmmny"};
+            int[] columnints = new int[]{3,5,3,1,3,3,3,3,3,3,3,3,3,3,3,3,3,3};
+            LinkedList<ColumnCellAttr> columnlist = new LinkedList<>();
+            List<String> columnkeylist  = new ArrayList<String>();
+            List<String> columnameslist  = new ArrayList<String>();
+            List<Integer> columnintlist = new ArrayList<Integer>();
+            for(int i =0;i<columnkeys.length;i++){
+                if("N".equals(showbnljjf) && ("bnjfnum".equals(columnkeys[i]) || "bnjfmny".equals(columnkeys[i]))){
+                    continue;
+                }
+                if("N".equals(showbnljdf) && ("bndfnum".equals(columnkeys[i]) || "bndfmny".equals(columnkeys[i]))){
+                    continue;
+                }
+                columnkeylist.add(columnkeys[i]);
+                columnintlist.add(columnints[i]);
+            }
+            for(int i=0;i<columnames.length;i++){
+                if("N".equals(showbnljjf) && (i == 7  || i== 13 || i ==14)){//本年累计借方
+                    continue;
+                }
+                if("N".equals(showbnljdf) && (i == 8 || i== 15 || i ==16) ){//本年累计贷方
+                    continue;
+                }
+                columnameslist.add(columnames[i]);
+                ColumnCellAttr attr = new ColumnCellAttr();
+                attr.setColumname(columnames[i]);
+                if(i==0 ||  i==1 || i==2|| i==3){
+                    attr.setRowspan(2);
+                }else if(i == 4){
+                    attr.setColspan(3);
+                }else if(i==5){
+                    attr.setColspan(2);
+                }else if(i==6){
+                    attr.setColspan(2);
+                }else if(i==7){
+                    attr.setColspan(2);
+                }else if(i==8){
+                    attr.setColspan(2);
+                }else if(i==9){
+                    attr.setColspan(3);
+                }
+
+                columnlist.add(attr);
+            }
+            columnints = new int[columnintlist.size()];
+            for(int i =0;i<columnintlist.size();i++){
+                columnints[i] = columnintlist.get(i);
+            }
+            printReporUtil.setTableHeadFount(new Font(printReporUtil.getBf(), Float.parseFloat(font), Font.NORMAL));//设置表头字体
+            if (type.equals("1"))
+                printReporUtil.printGroup(new HashMap<String, List<SuperVO>>(),bodyvos, "数 量 金 额 总 账", columnkeylist.toArray(new String[0]),
+                        columnameslist.toArray(new String[0]), columnlist,columnints, 0,null,pmap,tmap);// A4纸张打印
+            else if(type.equals("2")){
+                printReporUtil.printB5(new HashMap<String, List<SuperVO>>(),bodyvos, "数 量 金 额 总 账", columnkeylist.toArray(new String[0]),
+                        columnameslist.toArray(new String[0]), columnlist, columnints, 0,null,pmap,tmap);
+            }
+
+        } catch (DocumentException e) {
+            log.error("打印错误",e);
+        } catch (IOException e) {
+            log.error("打印错误",e);
+        }
+    }
+
+    private void setDefaultValue(NumMnyGlVO[] bodyvos, String pk_corp){
+        if(bodyvos != null && bodyvos.length > 0){
+            for(NumMnyGlVO vo : bodyvos){
+                vo.setPk_corp(pk_corp);
+            }
+        }
+    }
+
+    private void setExprotInfo(NmnyHzExcelField xsz){
+        xsz.setFields(xsz.getFields1());
+    }
+
+    //导出excel
+    @PostMapping("export/excel")
+    public void excelReport(@MultiRequestBody PrintParamVO printParamVO,
+                            @MultiRequestBody UserVO userVO, @MultiRequestBody CorpVO corpVO, HttpServletResponse response){
+//        QueryCondictionVO paramVO = new  QueryCondictionVO();
+//        paramVO = JsonUtils.convertValue(params, QueryCondictionVO.class);
+//        paramVO = (QueryCondictionVO) DzfTypeUtils.cast(getRequest(), paramVO);
+
+        String showbnljjf = printParamVO.getShowjf();
+        String showbnljdf = printParamVO.getShowdf();
+
+        String strlist = printParamVO.getList();
+//        JSONArray array = (JSONArray) JSON.parseArray(strlist);
+//        Map<String,String> bodymapping= FieldMapping.getFieldMapping(new PzmbbVO());
+//        NumMnyGlVO[] vo =DzfTypeUtils.cast(array,bodymapping, NumMnyGlVO[].class, JSONConvtoJAVA.getParserConfig());
+        NumMnyGlVO[] vo = JsonUtils.deserialize(strlist, NumMnyGlVO[].class);
+
+        String gs =  vo[0].getGs();
+        String qj =  vo[0].getTitlePeriod();
+        Excelexport2003<NumMnyGlVO> lxs = new Excelexport2003<NumMnyGlVO>();
+
+        String pk_corp = corpVO.getPk_corp();
+        String numStr = printParamVO.getNumstr();
+        String priceStr = printParamVO.getPricestr();
+        int num = StringUtil.isEmpty(numStr) ? 4 : Integer.parseInt(numStr);
+        int price = StringUtil.isEmpty(priceStr) ? 4 : Integer.parseInt(priceStr);
+
+        NmnyHzExcelField xsz = new NmnyHzExcelField(num, price,showbnljjf,showbnljdf);
+        setExprotInfo(xsz);
+        xsz.setNumMnyGlVOs(vo);
+        xsz.setQj(qj);
+        xsz.setCreator(userVO.getUser_name());
+        xsz.setCorpName(gs);
+        OutputStream toClient = null;
+        try {
+            response.reset();
+//			String filename = xsz.getExcelport2007Name();
+            String filename = xsz.getExcelport2003Name();
+            String formattedName = URLEncoder.encode(filename, "UTF-8");
+            response.addHeader("Content-Disposition", "attachment;filename=" + filename + ";filename*=UTF-8''" + formattedName);
+            toClient = new BufferedOutputStream(response.getOutputStream());
+            response.setContentType("application/vnd.ms-excel;charset=gb2312");
+            lxs.exportExcel(xsz, toClient);
+            toClient.flush();
+            response.getOutputStream().flush();
+        } catch (IOException e) {
+            log.error("excel导出错误",e);
+        }finally{
+            try{
+                if(toClient != null){
+                    toClient.close();
+                }
+            }catch(IOException e){
+                log.error("excel导出错误",e);
+            }
+            try{
+                if(response!=null && response.getOutputStream() != null){
+                    response.getOutputStream().close();
+                }
+            }catch(IOException e){
+                log.error("excel导出错误",e);
+            }
+        }
+        //日志记录
+//        writeLogRecord(LogRecordEnum.OPE_KJ_KMREPORT.getValue(),
+//                new StringBuffer().append("数量金额总账导出:")
+//                        .append(qj).toString(), ISysConstants.SYS_2);
+    }
+
 
 }
---
+++ zxkj-modules/zxkj-report/src/main/java/com/dzf/zxkj/report/excel/cwzb/NmnyHzExcelField.java
@@ -0,0 +1,197 @@
+package com.dzf.zxkj.report.excel.cwzb;
+
+import com.dzf.zxkj.common.lang.DZFDate;
+import com.dzf.zxkj.excel.param.Fieldelement;
+import com.dzf.zxkj.excel.param.IExceport;
+import com.dzf.zxkj.platform.model.report.NumMnyGlVO;
+import com.dzf.zxkj.report.utils.ReportUtil;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+/**
+ * 数量金额总账导出配置
+ * @author zpm
+ *
+ */
+public class NmnyHzExcelField implements IExceport<NumMnyGlVO> {
+	
+	private NumMnyGlVO[] NumMnyGlVOs = null;
+	
+	private String qj = null;
+	
+	private String now = DZFDate.getDate(new Date()).toString();
+	
+	private String creator = null;
+	
+	private String corpname = null;
+	
+	private Fieldelement[] fields = null;
+	
+	private int numPrecision;//数量精度
+	private int pricePrecision;//单价精度
+	
+	private String showbnljjf;//本年累计借方，显示
+	
+	private String showbnljdf;//本年累计贷方，显示
+	
+//	public NmnyHzExcelField(){
+//		numPrecision = 4;
+//		pricePrecision = 4;
+//	}
+	
+	public NmnyHzExcelField(int numPrecision, int pricePrecision,String showbnljjf,String showbnljdf){
+		this.numPrecision = numPrecision;
+		this.pricePrecision = pricePrecision;
+		this.showbnljjf = showbnljjf;
+		this.showbnljdf = showbnljdf;
+	}
+	
+	
+	
+	@Override
+	public String getExcelport2007Name() {
+		return "数量金额总账-"+ ReportUtil.formatQj(qj)+".xlsx";
+	}
+	
+	@Override
+	public String getExcelport2003Name() {
+		return "数量金额总账-"+ReportUtil.formatQj(qj)+".xls";
+	}
+
+	@Override
+	public String getExceportHeadName() {
+		return "数量金额总账";
+	}
+
+	@Override
+	public String getSheetName() {
+		return "数量金额总账";
+	}
+
+	@Override
+	public NumMnyGlVO[] getData() {
+		return NumMnyGlVOs;
+	}
+
+	@Override
+	public Fieldelement[] getFieldInfo() {
+		return fields;
+	}
+
+	public void setNumMnyGlVOs(NumMnyGlVO[] NumMnyGlVOs) {
+		this.NumMnyGlVOs = NumMnyGlVOs;
+	}
+
+	@Override
+	public String getQj() {
+		return qj;
+	}
+
+	@Override
+	public String getCreateSheetDate() {
+		return now;
+	}
+
+	@Override
+	public String getCreateor() {
+		return creator;
+	}
+
+	public void setQj(String qj) {
+		this.qj = qj;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	@Override
+	public String getCorpName() {
+		return corpname;
+	}
+	
+	public void setCorpName(String corpname){
+		this.corpname = corpname;
+	}
+	
+	public Fieldelement[] getFields1() {
+		List<Fieldelement> fieldlist = new ArrayList<Fieldelement>();
+		fieldlist.add(new Fieldelement("kmbm", "科目编码", false, 0, false));
+		fieldlist.add(new Fieldelement("kmbm", "科目编码", false, 0, false));
+		fieldlist.add(new Fieldelement("kmmc", "科目名称", false, 0, false));
+		fieldlist.add(new Fieldelement("dw", "计量单位", false, 0, false));
+		fieldlist.add(new Fieldelement("dir", "方向", false, 0, false));
+		fieldlist.add(new Fieldelement("qcnum", "期初数量", true, numPrecision, true));
+		fieldlist.add(new Fieldelement("qcprice", "期初单价", true, pricePrecision, true));
+		fieldlist.add(new Fieldelement("qcmny", "期初金额", true, 2, true));
+		fieldlist.add(new Fieldelement("bqjfnum", "本期借方数量", true, numPrecision, true));
+		fieldlist.add(new Fieldelement("bqjfmny", "本期借方金额", true, 2, true));
+		fieldlist.add(new Fieldelement("bqdfnum", "本期贷方数量", true, numPrecision, true));
+		fieldlist.add(new Fieldelement("bqdfmny", "本期贷方金额", true, 2, true));
+		if("Y".equals(showbnljjf)){
+			fieldlist.add(new Fieldelement("bnjfnum", "本年借方数量", true, numPrecision, true));
+			fieldlist.add(new Fieldelement("bnjfmny", "本年借方金额", true, 2, true));
+		}
+		if("Y".equals(showbnljdf)){
+			fieldlist.add(new Fieldelement("bndfnum", "本年贷方数量", true, numPrecision, true));
+			fieldlist.add(new Fieldelement("bndfmny", "本年贷方金额", true, 2, true));
+		}
+		fieldlist.add(new Fieldelement("qmnum", "期末数量", true, numPrecision, true));
+		fieldlist.add(new Fieldelement("qmprice", "期末单价", true, pricePrecision, true));
+		fieldlist.add(new Fieldelement("qmmny", "期末金额", true, 2, true));
+		return fieldlist.toArray(new Fieldelement[0]);
+	}
+
+	public Fieldelement[] getFields2() {
+
+		return new Fieldelement[]{
+				new Fieldelement("beginqj", "起始时间",false,0,false),
+				new Fieldelement("endqj", "截止时间",false,0,false),
+				new Fieldelement("kmmc", "科目名称",false,0,false),
+				new Fieldelement("spmc", "存货名称",false,0,false),
+				new Fieldelement("qcnum", "期初数量",true, numPrecision,true),
+				new Fieldelement("qcprice", "期初单价",true, pricePrecision,true),
+				new Fieldelement("qcmny", "期初金额",true,2,true),
+				new Fieldelement("bqjfnum", "本期借方数量",true, numPrecision,true),
+				new Fieldelement("bqjfmny", "本期借方金额",true,2,true),
+				new Fieldelement("bqdfnum", "本期贷方数量",true, numPrecision,true),
+				new Fieldelement("bqdfmny", "本期贷方金额",true,2,true),
+				new Fieldelement("bnjfnum", "本年借方数量",true, numPrecision,true),
+				new Fieldelement("bnjfmny", "本年借方金额",true,2,true),
+				new Fieldelement("bndfnum", "本年贷方数量",true, numPrecision,true),
+				new Fieldelement("bndfmny", "本年贷方金额",true,2,true),
+				new Fieldelement("qmnum", "期末数量",true, numPrecision,true),
+				new Fieldelement("qmprice", "期末单价",true, pricePrecision,true),
+				new Fieldelement("qmmny", "期末金额",true,2,true)
+		};
+	}
+	
+
+	public String getShowbnljjf() {
+		return showbnljjf;
+	}
+
+	public void setShowbnljjf(String showbnljjf) {
+		this.showbnljjf = showbnljjf;
+	}
+
+	public String getShowbnljdf() {
+		return showbnljdf;
+	}
+
+	public void setShowbnljdf(String showbnljdf) {
+		this.showbnljdf = showbnljdf;
+	}
+
+	public void setFields(Fieldelement[] fields) {
+		this.fields = fields;
+	}
+
+	@Override
+	public boolean[] isShowTitDetail() {
+		return new boolean[]{true,true,false};
+	}
+}
