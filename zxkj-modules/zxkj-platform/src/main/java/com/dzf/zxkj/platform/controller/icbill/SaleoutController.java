3b1513d9db1b8ddadd7dbef6d9fbbeadf2e150f0
zhw <zhenghongwei@dazhangfang.com>
2019-12-26 14:08:10 +0800

    销售出库复制

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/icbill/SaleoutController.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/icbill/SaleoutController.java
@@ -269,8 +269,10 @@ public class SaleoutController{
 					child.setAttributeValue("imppzh", null);
 					child.setAttributeValue("pzh", null);
 					child.setAttributeValue("pk_voucher_b", null);
-					child.setAttributeValue("ncost", DZFDouble.ZERO_DBL);
-					child.setAttributeValue("vdef1", DZFDouble.ZERO_DBL);
+                    if (vo.getCbusitype() == null || (IcConst.XSTYPE.equals(vo.getCbusitype()))) {
+                        child.setAttributeValue("ncost", DZFDouble.ZERO_DBL);
+                        child.setAttributeValue("vdef1", DZFDouble.ZERO_DBL);
+                    }
 				}
 			}
 		}
