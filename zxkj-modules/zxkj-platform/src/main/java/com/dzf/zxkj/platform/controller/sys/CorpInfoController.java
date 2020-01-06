ef71513ee17ca1b3ae49ff75814c548d5491cef4
renyang <admin@DESKTOP-BDERPMU>
2020-01-06 20:09:22 +0800

    进销项本方公司信息参照

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/sys/CorpInfoController.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/sys/CorpInfoController.java
@@ -2,15 +2,18 @@ package com.dzf.zxkj.platform.controller.sys;
 
 import com.dzf.zxkj.base.controller.BaseController;
 import com.dzf.zxkj.common.constant.IcCostStyle;
+import com.dzf.zxkj.common.entity.Grid;
 import com.dzf.zxkj.common.entity.Json;
 import com.dzf.zxkj.common.entity.ReturnData;
 import com.dzf.zxkj.common.lang.DZFDate;
+import com.dzf.zxkj.common.utils.CodeUtils1;
 import com.dzf.zxkj.common.utils.IGlobalConstants;
 import com.dzf.zxkj.jackson.annotation.MultiRequestBody;
 import com.dzf.zxkj.platform.model.image.ImageCommonPath;
 import com.dzf.zxkj.platform.model.sys.CorpVO;
 import com.dzf.zxkj.platform.model.sys.CustServVO;
 import com.dzf.zxkj.platform.model.sys.UserVO;
+import com.dzf.zxkj.platform.service.pzgl.ICorpInfoService;
 import com.dzf.zxkj.platform.service.sys.ICorpService;
 import com.dzf.zxkj.platform.service.sys.ICustServService;
 import com.dzf.zxkj.platform.util.SystemUtil;
@@ -20,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -32,6 +36,8 @@ public class CorpInfoController extends BaseController {
     private ICorpService corpService;
     @Autowired
     private ICustServService custServServiceImpl;
+    @Autowired
+    private ICorpInfoService corpInfoSer;
 
     @RequestMapping("getCorpServiceInfo")
     public ReturnData getCorpServiceInfo(@RequestParam String corpId) {
@@ -84,5 +90,27 @@ public class CorpInfoController extends BaseController {
             log.error("获取公司信息失败", e);
         }
         return ReturnData.ok().data(json);
+    }
+    /**
+     * 查找公司信息
+     * 会计端-企业信息调用
+     */
+    @RequestMapping("/queryCorpInfo")
+    public ReturnData queryCorpInfo(String id){
+        Grid grid = new Grid();
+        try {
+              CorpVO[] corpvo = corpInfoSer.queryCorpInfo(id);
+              log.info("企业信息查询成功！");
+              for(CorpVO vo:corpvo){
+                  vo.setPhone1(CodeUtils1.deCode(vo.getPhone1()));
+                  vo.setUnitname(CodeUtils1.deCode(vo.getUnitname()));
+               }
+               grid.setRows(Arrays.asList(corpvo));
+               grid.setMsg("查询成功!");
+               grid.setSuccess(true);
+        }  catch (Exception e) {
+            log.error("获取公司信息失败", e);
+        }
+        return ReturnData.ok().data(grid);
     }
 }
