2aa90fea4d8d3f94a970155e180ffc5afb369b45
zhw <zhenghongwei@dazhangfang.com>
2020-01-19 16:28:30 +0800

    权限校验公司

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/service/common/impl/SecurityServiceImpl.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/service/common/impl/SecurityServiceImpl.java
@@ -2,7 +2,9 @@ package com.dzf.zxkj.platform.service.common.impl;
 
 import com.dzf.zxkj.base.dao.SingleObjectBO;
 import com.dzf.zxkj.base.exception.BusinessException;
+import com.dzf.zxkj.base.framework.SQLParameter;
 import com.dzf.zxkj.base.framework.processor.ColumnProcessor;
+import com.dzf.zxkj.base.utils.DZfcommonTools;
 import com.dzf.zxkj.common.model.SuperVO;
 import com.dzf.zxkj.common.utils.SqlUtil;
 import com.dzf.zxkj.common.utils.StringUtil;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service("ic_securityserv")
@@ -50,24 +53,37 @@ public class SecurityServiceImpl implements ISecurityService {
         if(vos == null || vos.length==0){
             return ;
         }
-        String pkFieldName = vos[0].getPKFieldName();
-        List<String> pkList = Arrays.stream(vos).filter(item -> !StringUtil.isEmpty((String)item.getAttributeValue(pkFieldName)))
-                .map(item -> {
-                    return (String)item.getAttributeValue(pkFieldName);
-                }).collect(Collectors.toList());
+        Map<String,List<SuperVO>> map = DZfcommonTools.hashlizeObject( Arrays.asList(vos), new String[]{"pk_corp"});
+        String pk_corp = null;
+        List<SuperVO> list= null;
+        for (Map.Entry<String, List<SuperVO>> entry : map.entrySet()) {
+            pk_corp = entry.getKey();
+            if(StringUtil.isEmpty(pk_corp))
+                throw new BusinessException("出现数据中公司不存在问题！");
+            list = entry.getValue();
+            if(list == null || list.isEmpty())
+                continue;
+            String pkFieldName =list.get(0).getPKFieldName();
+            List<String> pkList =list.stream().filter(item -> !StringUtil.isEmpty((String)item.getAttributeValue(pkFieldName)))
+                    .map(item -> {
+                        return (String)item.getAttributeValue(pkFieldName);
+                    }).collect(Collectors.toList());
 //        pkList = pkList.stream().distinct().filter(v -> !StringUtil.isEmpty(v)).collect(Collectors.toList());
-        if(pkList.size() ==0){
-            return;
-        }
-        StringBuffer sql = new StringBuffer(" select count(0) from ");
-        sql.append( vos[0].getTableName());
-        sql.append(" where nvl(dr,0) = 0 and " );
-        sql.append(SqlUtil.buildSqlForIn(vos[0].getPKFieldName(), pkList.toArray(new String[pkList.size()])));
+            if(pkList.size() ==0){
+                return;
+            }
+            StringBuffer sql = new StringBuffer(" select count(0) from ");
+            SQLParameter sp = new SQLParameter();
+            sql.append(list.get(0).getTableName());
+            sp.addParam(pk_corp);
+            sql.append(" where nvl(dr,0) = 0 and pk_corp = ? and " );
+            sql.append(SqlUtil.buildSqlForIn(pkFieldName, pkList.toArray(new String[pkList.size()])));
 
-        int count =  new Integer(singleObjectBO.executeQuery(sql.toString(), null, new ColumnProcessor()).toString()).intValue();
-       if(count != vos.length){
-           throw new BusinessException("出现数据问题，请重新操作！");
-       }
+            int count =  new Integer(singleObjectBO.executeQuery(sql.toString(), sp, new ColumnProcessor()).toString()).intValue();
+            if(count != list.size()){
+                throw new BusinessException("出现数据问题，请重新操作！");
+            }
+        }
     }
     private void checkSecurityForCorp(List<String> corpList,String cuserid){
 
