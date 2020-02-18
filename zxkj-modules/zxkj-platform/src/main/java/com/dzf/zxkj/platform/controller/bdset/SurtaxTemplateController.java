5bfa6f977364702478e659b04e1208111b9eb42e
liubj <liubinjie@dazhangfang.com>
2020-02-18 13:53:33 +0800

    日志

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/bdset/SurtaxTemplateController.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/bdset/SurtaxTemplateController.java
@@ -1,7 +1,9 @@
 package com.dzf.zxkj.platform.controller.bdset;
 
+import com.dzf.zxkj.base.controller.BaseController;
 import com.dzf.zxkj.common.entity.Json;
 import com.dzf.zxkj.common.entity.ReturnData;
+import com.dzf.zxkj.common.enums.LogRecordEnum;
 import com.dzf.zxkj.platform.model.bdset.SurtaxTemplateVO;
 import com.dzf.zxkj.platform.model.sys.CorpVO;
 import com.dzf.zxkj.platform.service.bdset.ISurtaxTemplateService;
@@ -13,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/bdset/gl_surtaxtempact")
-public class SurtaxTemplateController {
+public class SurtaxTemplateController extends BaseController {
     @Autowired
     private ISurtaxTemplateService gl_surtaxtempserv;
 
@@ -25,6 +27,8 @@ public class SurtaxTemplateController {
                 : gl_surtaxtempserv.query(corp);
         json.setRows(list);
         json.setSuccess(true);
+        writeLogRecord(LogRecordEnum.OPE_KJ_BDSET,
+                "计提附加税模板设置");
         return ReturnData.ok().data(json);
     }
 
