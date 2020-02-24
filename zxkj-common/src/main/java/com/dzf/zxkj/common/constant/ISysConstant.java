83c836883d6ac19e5db6dd1ea820faf3371e4861
高志新 <gzhx>
2020-02-25 00:13:33 +0800

    打印日志

--- zxkj-common/src/main/java/com/dzf/zxkj/common/constant/ISysConstant.java
+++ zxkj-common/src/main/java/com/dzf/zxkj/common/constant/ISysConstant.java
@@ -4,6 +4,6 @@ public interface ISysConstant {
     String LOGIN_PK_CORP = "X-Login-Corp";
     String LOGIN_USER_ID = "X-Login-User";
     String LOGIN_DATE = "X-Login-Date";
-    String CLIENT_ID = "CLIENT_ID";
+    String CLIENT_ID = "X-CLIENT-ID";
     String TOKEN = "X-ACCESS-TOKEN";
 }
