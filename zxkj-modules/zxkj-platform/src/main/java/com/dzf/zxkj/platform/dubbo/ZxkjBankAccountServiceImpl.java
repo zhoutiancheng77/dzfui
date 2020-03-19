7f4edd3cc1325f8c2cc32c2bee4b465164cab975
高志新 <gzhx>
2020-03-19 15:38:24 +0800

    对外接口

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/dubbo/ZxkjBankAccountServiceImpl.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/dubbo/ZxkjBankAccountServiceImpl.java
@@ -5,7 +5,10 @@ import com.dzf.zxkj.bankaccount.model.ReturnData;
 import com.dzf.zxkj.bankaccount.model.SignOnlineVO;
 import com.dzf.zxkj.bankaccount.service.IBankAccountService;
 import com.dzf.zxkj.bankaccount.system.BankAccountConstant;
+import com.dzf.zxkj.platform.service.bdset.IYHZHService;
+import com.dzf.zxkj.platform.service.report.IYntBoPubUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -13,10 +16,38 @@ import org.springframework.stereotype.Service;
 @org.apache.dubbo.config.annotation.Service(version = BankAccountConstant.version, timeout = Integer.MAX_VALUE)
 public class ZxkjBankAccountServiceImpl implements IBankAccountService {
 
+
+    @Autowired
+    private IYHZHService gl_yhzhserv;
+
+    @Autowired
+    private IYntBoPubUtil yntBoPubUtil;
+
     @Override
     public ReturnData saveBankAccount(BankAccountVO bankAccountVO) {
+        com.dzf.zxkj.platform.model.bdset.BankAccountVO bankAccount = new com.dzf.zxkj.platform.model.bdset.BankAccountVO();
+        bankAccount.setBanktype("中国工商银行");
+        bankAccount.setBankaccount(bankAccountVO.getBankcode());
+        bankAccount.setLy("0"); //签约生成
+        bankAccount.setBankTypeCode("1");
+        bankAccount.setZhlx("0");
+        bankAccount.setCoperatorid(bankAccountVO.getVapplyuserid());
+        bankAccount.setPk_corp(bankAccountVO.getInnercode());
 
-        return null;
+        String invcode = yntBoPubUtil.getYhzhCode(bankAccountVO.getInnercode());
+
+        bankAccount.setCode(invcode);
+
+        ReturnData returnData = new ReturnData();
+
+        try{
+            gl_yhzhserv.save(bankAccount);
+            returnData.setCode("200");
+        }catch (Exception e){
+            returnData.setCode("500");
+            returnData.setMessage(e.getMessage());
+        }
+        return returnData;
     }
 
     @Override
