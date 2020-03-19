4e816fdc25b92c8e1293aa45b5a86ff7a68ad6d1
zhw <zhenghongwei@dazhangfang.com>
2020-03-19 23:38:43 +0800

    工资表打印

--- zxkj-tools/zxkj-excel-export-tool/src/main/java/com/dzf/zxkj/pdf/PrintReporUtil.java
+++ zxkj-tools/zxkj-excel-export-tool/src/main/java/com/dzf/zxkj/pdf/PrintReporUtil.java
@@ -1480,7 +1480,7 @@ public class PrintReporUtil {
         int total = kmmap.size();
         for (Map.Entry<String, List<SuperVO>> kmEntry : kmmap.entrySet()) { //
             if ("资 产 负 债 表".equals(titlename) || "利 润 表".equals(titlename)
-                    || "分 部 利 润 表".equals(titlename) || "现 金 流 量 表".equals(titlename)) {
+                    || "分 部 利 润 表".equals(titlename) || "现 金 流 量 表".equals(titlename) || titlename.startsWith("工 资 表")) {
                 tmap.put("期间", kmEntry.getKey());
             }
             List<SuperVO> kmList = kmEntry.getValue(); // 得到当前科目 所对应的 数据
@@ -1540,6 +1540,9 @@ public class PrintReporUtil {
                                          PdfWriter writer, Map<String, Object> para) throws DocumentException {
         int pageNum = 1;
         for (Map.Entry<String, List<SuperVO>> kmEntry : kmmap.entrySet()) { //
+            if (titlename.startsWith("工 资 表")) {
+                tmap.put("期间", kmEntry.getKey());
+            }
             List<SuperVO> kmList = kmEntry.getValue(); // 得到当前科目 所对应的 数据
             Object[] objs = putRptHeadMap(tmap, titlename, kmList, para, invmaps, 1);//B5纸张
             tmap = (Map<String, String>) objs[0];
