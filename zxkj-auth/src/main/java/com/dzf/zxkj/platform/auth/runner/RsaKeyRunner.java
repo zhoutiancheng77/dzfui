77d23c8c8694b113264999fe8b164329422e8f03
高志新 <gzhx>
2020-04-01 11:11:52 +0800

    版本回退

--- zxkj-auth/src/main/java/com/dzf/zxkj/platform/auth/runner/RsaKeyRunner.java
+++ zxkj-auth/src/main/java/com/dzf/zxkj/platform/auth/runner/RsaKeyRunner.java
@@ -1,9 +1,5 @@
 package com.dzf.zxkj.platform.auth.runner;
 
-import com.alicp.jetcache.Cache;
-import com.alicp.jetcache.CacheGetResult;
-import com.alicp.jetcache.anno.CacheType;
-import com.alicp.jetcache.anno.CreateCache;
 import com.dzf.zxkj.platform.auth.config.RsaKeyConfig;
 import com.dzf.zxkj.platform.auth.utils.RsaKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @Component
 public class RsaKeyRunner implements CommandLineRunner {
@@ -19,24 +14,9 @@ public class RsaKeyRunner implements CommandLineRunner {
     @Autowired
     private RsaKeyConfig rsaKeyConfig;
 
-    @CreateCache(name = "zxkj:platform:rsaKey", cacheType = CacheType.REMOTE, expire = 30, timeUnit = TimeUnit.DAYS)
-    private Cache<String, Map<String, byte[]>> rsaKeyCache;
-
-
     @Override
     public void run(String... args) throws Exception {
-
-        Map<String, byte[]> keyMap;
-
-        CacheGetResult<Map<String, byte[]>> result = rsaKeyCache.GET("rsaKey");
-
-        if(result.isSuccess()){
-            keyMap = rsaKeyCache.get("rsaKey");
-        }else{
-            keyMap = RsaKeyUtil.getInstance().generateKey(rsaKeyConfig.getUserSecret());
-            rsaKeyCache.put("rsaKey", keyMap);
-        }
-
+        Map<String, byte[]> keyMap = RsaKeyUtil.getInstance().generateKey(rsaKeyConfig.getUserSecret());
         rsaKeyConfig.setUserPriKey(keyMap.get("pri"));
         rsaKeyConfig.setUserPubKey(keyMap.get("pub"));
     }
