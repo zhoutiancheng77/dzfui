0cb54dc28c4ee412dcafb95301fd8aa092d1a932
高志新 <gzhx>
2020-04-13 14:10:39 +0800

    redis集群緩存

---
+++ zxkj-common/src/main/java/com/dzf/zxkj/common/constant/RedisCacheConstant.java
@@ -0,0 +1,7 @@
+package com.dzf.zxkj.common.constant;
+
+@SuppressWarnings("all")
+public class RedisCacheConstant {
+    public static final String YHZH_NAME = "zxkj-yhzh";
+    public static final Integer YHZH_EXPIRE = 7;
+}
