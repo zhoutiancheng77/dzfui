1235ba3373c6db9a6297ea5f013b4fe71460632b
高志新 <gzhx>
2020-04-13 15:14:44 +0800

    会计科目缓存准备 暂时做不了

---
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/dao/YntCpaccountDao.java
@@ -0,0 +1,32 @@
+package com.dzf.zxkj.platform.dao;
+
+import com.dzf.zxkj.base.dao.SingleObjectBO;
+import com.dzf.zxkj.base.framework.SQLParameter;
+import com.dzf.zxkj.platform.model.bdset.YntCpaccountVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+@Repository
+public class YntCpaccountDao {
+    @Autowired
+    private SingleObjectBO singleObjectBO;
+
+    public YntCpaccountVO[] queryByPkCorp(String pk_corp){
+        SQLParameter sp = new SQLParameter();
+        sp.addParam(pk_corp);
+        YntCpaccountVO[] cpvos = (YntCpaccountVO[])singleObjectBO.queryByCondition(YntCpaccountVO.class, " pk_corp = ? and nvl(dr,0) = 0 ", sp);
+        if(cpvos != null && cpvos.length > 0){
+            Arrays.sort(cpvos, new Comparator<YntCpaccountVO>() {
+                @Override
+                public int compare(YntCpaccountVO arg0, YntCpaccountVO arg1) {
+                    return arg0.getAccountcode().compareTo(arg1.getAccountcode());
+                }
+            });
+        }
+        return cpvos == null ? new YntCpaccountVO[0] : cpvos;
+    }
+
+}
