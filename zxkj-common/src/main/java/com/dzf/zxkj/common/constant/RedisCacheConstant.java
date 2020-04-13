1728ba9f003dc4bd954e2134cd008cf9e222f0ce
高志新 <gzhx>
2020-04-13 14:16:27 +0800

    redis集群緩存

--- zxkj-common/src/main/java/com/dzf/zxkj/common/constant/RedisCacheConstant.java
+++ zxkj-common/src/main/java/com/dzf/zxkj/common/constant/RedisCacheConstant.java
@@ -2,6 +2,15 @@ package com.dzf.zxkj.common.constant;
 
 @SuppressWarnings("all")
 public class RedisCacheConstant {
+    /**
+     *  常规设置银行账户缓存
+     */
     public static final String YHZH_NAME = "zxkj-yhzh";
     public static final Integer YHZH_EXPIRE = 7;
+
+    /**
+     *  常规设置参数设置缓存
+     */
+    public static final String PARAMETER_NAME = "zxkj-parameter-set";
+    public static final Integer PARAMETER_EXPIRE = 7;
 }
