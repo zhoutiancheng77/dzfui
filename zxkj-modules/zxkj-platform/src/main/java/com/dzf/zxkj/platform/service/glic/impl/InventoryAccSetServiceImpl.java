7e0d87e69d5e40a9214817dca6ed219ba35f236e
zhw <zhenghongwei@dazhangfang.com>
2020-04-21 10:48:37 +0800

    存货设置

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/service/glic/impl/InventoryAccSetServiceImpl.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/service/glic/impl/InventoryAccSetServiceImpl.java
@@ -44,8 +44,8 @@ public class InventoryAccSetServiceImpl implements IInventoryAccSetService {
 		if(bodyvos != null && bodyvos.length>0){
 			body = bodyvos[0];
 		}else{
-			body = new InventorySetVO();
-			body.setChcbjzfs(InventoryConstant.IC_FZMXHS);
+//			body = new InventorySetVO();
+//			body.setChcbjzfs(InventoryConstant.IC_FZMXHS);
 		}
 		return body;
 	}
