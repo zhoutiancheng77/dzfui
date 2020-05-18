44c43ceeb44ae30fb6c98660cf10926436a43a43
高志新 <gzhx>
2020-05-18 10:43:30 +0800

    数据备份与恢复单独处理

--- zxkj-modules/zxkj-backup/src/main/java/com/dzf/zxkj/backup/service/ISecretKeyService.java
+++ zxkj-modules/zxkj-backup/src/main/java/com/dzf/zxkj/backup/service/ISecretKeyService.java
@@ -8,36 +8,38 @@ import java.util.Map;
 
 public interface ISecretKeyService {
 
-	
-	/**
-	 * 查询秘钥信息（公司+来源+版本号）
-	 * @param pk_corp
-	 * @param versionno
-	 * @param versiondate 时间戳类型
-	 * @return
-	 * @throws DZFWarpException
-	 */
-	public List<SecretKeyVo> querySecretKeyFromNo(String pk_corp, Integer isourcesys, String versionno) throws DZFWarpException;
+
+    /**
+     * 查询秘钥信息（公司+来源+版本号）
+     *
+     * @param pk_corp
+     * @param versionno
+     * @param versiondate 时间戳类型
+     * @return
+     * @throws DZFWarpException
+     */
+    List<SecretKeyVo> querySecretKeyFromNo(String pk_corp, Integer isourcesys, String versionno) throws DZFWarpException;
 
 
-	
-	/**
-	 * 查询秘钥信息 (公司+来源+版本日期) 来确定唯一
-	 * @param pk_corp
-	 * @param versionno
-	 * @param versiondate 时间戳类型
-	 * @return
-	 * @throws DZFWarpException
-	 */
-	public List<SecretKeyVo>  querySecretKeyFromDate(String pk_corp, Integer isourcesys, String versionbegdate, String versino) throws DZFWarpException;
+    /**
+     * 查询秘钥信息 (公司+来源+版本日期) 来确定唯一
+     *
+     * @param pk_corp
+     * @param versionno
+     * @param versiondate 时间戳类型
+     * @return
+     * @throws DZFWarpException
+     */
+    List<SecretKeyVo> querySecretKeyFromDate(String pk_corp, Integer isourcesys, String versionbegdate, String versino) throws DZFWarpException;
 
-	
-	/**
-	 * 获取文件内容（RSA）
-	 * @param filename
-	 * @return
-	 * @throws DZFWarpException
-	 */
-	public Map<String, String> getRsaCodeValue(String filename) throws DZFWarpException;
-	
+
+    /**
+     * 获取文件内容（RSA）
+     *
+     * @param filename
+     * @return
+     * @throws DZFWarpException
+     */
+    Map<String, String> getRsaCodeValue(String filename) throws DZFWarpException;
+
 }
