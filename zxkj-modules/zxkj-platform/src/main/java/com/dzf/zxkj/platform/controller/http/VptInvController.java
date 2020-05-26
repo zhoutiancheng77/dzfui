e0ab3cce7e56782de1f4b7d7984c3a3b6e6aa90d
wangzhaoning <wangzhaoning@dazhangfang.com>
2020-05-26 16:59:15 +0800

    1

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/http/VptInvController.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/controller/http/VptInvController.java
@@ -1,5 +1,6 @@
 package com.dzf.zxkj.platform.controller.http;
 
+import com.alibaba.fastjson.JSON;
 import com.dzf.zxkj.base.controller.BaseController;
 import com.dzf.zxkj.base.exception.BusinessException;
 import com.dzf.zxkj.common.entity.Json;
@@ -11,9 +12,12 @@ import com.dzf.zxkj.platform.util.zncs.CommonXml;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @RestController
 @RequestMapping("/vpt/vptinv")
@@ -24,10 +28,14 @@ public class VptInvController extends BaseController {
     private IVptInvService vptservice;
 
     @PostMapping("/register")
-    public ReturnData<Json> register(@RequestBody PiaoTongReqVO reqvo){
+    public ReturnData<Json> register(HttpServletRequest request){
         PiaoTongResVO resvo = new PiaoTongResVO();
 
         try {
+            String param = getParm(request);//获取参数
+            printLog(param);
+            PiaoTongReqVO reqvo = JSON.parseObject(param, PiaoTongReqVO.class);
+
             buildResVO(resvo, reqvo);
             vptservice.saveRegis(reqvo);
             resvo.setCode(CommonXml.rtnsucccode);
@@ -42,6 +50,31 @@ public class VptInvController extends BaseController {
         return ReturnData.ok().data(resvo);
     }
 
+    private String getParm(HttpServletRequest request) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new InputStreamReader(request.getInputStream(), "UTF-8"));
+        } catch (Exception e) {
+
+        }
+        String line = null;
+        StringBuilder sb = new StringBuilder();
+        try {
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+
+        return sb.toString();
+
+    }
+
+    private void printLog(String param){
+        log.info(param);
+    }
+
     private void buildResVO(PiaoTongResVO resvo, PiaoTongReqVO reqvo){
         resvo.setCode(CommonXml.rtnfailcode);
         resvo.setSign(reqvo.getSign());
@@ -49,7 +82,7 @@ public class VptInvController extends BaseController {
     }
 
     @PostMapping("/invoiceinfo")
-    public ReturnData<Json> invoice(@RequestBody PiaoTongReqVO reqvo){
+    public ReturnData<Json> invoice(HttpServletRequest request){
         PiaoTongResVO resvo = new PiaoTongResVO();
 
 //        StringBuffer sf = new StringBuffer();
@@ -63,6 +96,9 @@ public class VptInvController extends BaseController {
 //        reqvo.setContent(sf.toString());
 
         try {
+            String param = getParm(request);//获取参数
+            printLog(param);
+            PiaoTongReqVO reqvo = JSON.parseObject(param, PiaoTongReqVO.class);
             buildResVO(resvo, reqvo);
             vptservice.updateInvoice(reqvo);
             resvo.setCode(CommonXml.rtnsucccode);
