2cb59f0be1d08199e40478b800b3aafb817c6e89
yinyixuan <329327140@qq.com>
2020-06-04 10:51:41 +0800

    APP

---
+++ zxkj-modules/zxkj-app/src/main/java/com/dzf/zxkj/app/utils/AppkeyUtil.java
@@ -0,0 +1,26 @@
+package com.dzf.zxkj.app.utils;
+
+import com.dzf.zxkj.common.utils.BeanHelper;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+public class AppkeyUtil {
+
+    public static void setAppValue(Map<String,Object> map,Object svo){
+        for (Field field : svo.getClass().getDeclaredFields())
+        {
+            JsonProperty annotation = field.getAnnotation(JsonProperty.class);
+            String fieldkey = field.getName();
+            if (null != annotation)
+            {
+                String ankey = annotation.value();
+                Object obj = map.get(ankey);
+                BeanHelper.setProperty(svo, fieldkey, obj);
+            }else {
+               // BeanHelper.setProperty(svo, fieldkey ,map.get(fieldkey));
+            }
+        }
+    }
+}
