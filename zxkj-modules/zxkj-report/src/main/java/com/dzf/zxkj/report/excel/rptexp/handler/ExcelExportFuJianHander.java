fa0eaf13720bc594fd2a3db824da0ecd82da86c7
zhangj <zhangj@admin-PC>
2020-06-04 18:04:01 +0800

    导出

--- zxkj-modules/zxkj-report/src/main/java/com/dzf/zxkj/report/excel/rptexp/handler/ExcelExportFuJianHander.java
+++ zxkj-modules/zxkj-report/src/main/java/com/dzf/zxkj/report/excel/rptexp/handler/ExcelExportFuJianHander.java
@@ -18,23 +18,39 @@ import java.util.Map;
 public class ExcelExportFuJianHander extends ExcelExportHander implements OneWorkBookKj2007Excel, OneWorkBookKj2013Excel {
     @Override
     public Workbook createWorkBookKj2007(Map<String, String> lrbTaxVoMap, Map<String, String> zcfzTaxVoMap, Map<String, String> xjllTaxVoMap, Map<String, LrbVO> lrbVOMap, Map<String, XjllbVO> xjllbVOMap, Map<String, ZcFzBVO> zcFzBVOMap) throws Exception {
-        Resource resource = ResourceUtil.get(ExportTemplateEnum.FUJIAN, ResourceUtil.ResourceEnum.KJ2007ALL);
-        Workbook workbook = WorkbookFactory.create(resource.getInputStream());
-        //资产负债表
-        Sheet sheet = workbook.getSheetAt(1);
-        handleZcfzbSheet(sheet, zcfzTaxVoMap, zcFzBVOMap, 6, new Integer[]{2, 3, 4, 5, 6, 7},new String[]{"qmye1","ncye1","qmye2","ncye2"});
-        //利润表
-        sheet = workbook.getSheetAt(2);
-        handleLrbSheet(sheet, lrbTaxVoMap,lrbVOMap, 5, new Integer[]{2,3,4}, new String[]{"bnljje","lastyear_bnljje"});
-        //现金流量表
-        sheet = workbook.getSheetAt(3);
-        handleXjllSheet(sheet, xjllTaxVoMap, xjllbVOMap, 6, new Integer[]{2,3,4}, new String[]{"sqje","sqje_last"});
-        return workbook;
+//        Resource resource = ResourceUtil.get(ExportTemplateEnum.FUJIAN, ResourceUtil.ResourceEnum.KJ2007ALL);
+//        Workbook workbook = WorkbookFactory.create(resource.getInputStream());
+//        //资产负债表
+//        Sheet sheet = workbook.getSheetAt(1);
+//        handleZcfzbSheet(sheet, zcfzTaxVoMap, zcFzBVOMap, 6, new Integer[]{2, 3, 4, 5, 6, 7},new String[]{"qmye1","ncye1","qmye2","ncye2"});
+//        //利润表
+//        sheet = workbook.getSheetAt(2);
+//        handleLrbSheet(sheet, lrbTaxVoMap,lrbVOMap, 5, new Integer[]{2,3,4}, new String[]{"bnljje","lastyear_bnljje"});
+//        //现金流量表
+//        sheet = workbook.getSheetAt(3);
+//        handleXjllSheet(sheet, xjllTaxVoMap, xjllbVOMap, 6, new Integer[]{2,3,4}, new String[]{"sqje","sqje_last"});
+//        return workbook;
+        // 默认走20196号，不走老版了
+        return createWorkBookKj2007(lrbTaxVoMap, zcfzTaxVoMap, xjllTaxVoMap, lrbVOMap, xjllbVOMap, zcFzBVOMap, "20196");
     }
 
     @Override
     public Workbook createWorkBookKj2007(Map<String, String> lrbTaxVoMap, Map<String, String> zcfzTaxVoMap, Map<String, String> xjllTaxVoMap, Map<String, LrbVO> lrbVOMap, Map<String, XjllbVO> xjllbVOMap, Map<String, ZcFzBVO> zcFzBVOMap, String versionno) throws Exception {
-        return null;
+        Resource resource = ResourceUtil.get(ExportTemplateEnum.BEIJING, ResourceUtil.ResourceEnum.KJ2007ALL,versionno);
+        Workbook workbook = WorkbookFactory.create(resource.getInputStream());
+        // 资产负债表
+        Sheet sheet = workbook.getSheetAt(1);
+        handleZcfzbSheet(sheet, zcfzTaxVoMap, zcFzBVOMap, 6, new Integer[] { 1, 3, 4, 5, 7, 8},
+                new String[] { "qmye1", "ncye1", "qmye2", "ncye2" });
+        // 利润表
+        sheet = workbook.getSheetAt(2);
+        handleLrbSheet(sheet, lrbTaxVoMap, lrbVOMap, 6, new Integer[] { 1, 2, 3 },
+                new String[] { "bnljje", "lastyear_bnljje" });
+        // 现金流量表
+        sheet = workbook.getSheetAt(3);
+        handleXjllSheet(sheet, xjllTaxVoMap, xjllbVOMap, 6, new Integer[] { 1, 2, 3 },
+                new String[] { "sqje", "sqje_last" });
+        return workbook;
     }
 
     @Override
--- zxkj-modules/zxkj-report/src/main/java/com/dzf/zxkj/report/excel/rptexp/handler/ExcelExportGuiZhouHander.java
+++ zxkj-modules/zxkj-report/src/main/java/com/dzf/zxkj/report/excel/rptexp/handler/ExcelExportGuiZhouHander.java
@@ -18,53 +18,69 @@ import java.util.Map;
 public class ExcelExportGuiZhouHander extends ExcelExportHander implements OneWorkBookKj2007Excel, OneWorkBookKj2013Excel {
     @Override
     public Workbook createWorkBookKj2007(Map<String, String> lrbTaxVoMap, Map<String, String> zcfzTaxVoMap, Map<String, String> xjllTaxVoMap, Map<String, LrbVO> lrbVOMap, Map<String, XjllbVO> xjllbVOMap, Map<String, ZcFzBVO> zcFzBVOMap) throws Exception {
-        Resource resource = ResourceUtil.get(ExportTemplateEnum.GUIZHOU, ResourceUtil.ResourceEnum.KJ2007ALL);
-        Workbook workbook = WorkbookFactory.create(resource.getInputStream());
-
-        Sheet sheet = workbook.getSheetAt(0);
-        sheet.getRow(4).getCell(1).setCellValue(getNsrsbh());
-        sheet.getRow(5).getCell(1).setCellValue(getNsrmc());
-
-        sheet.getRow(6).getCell(1).setCellValue(getBeginQj().substring(0,4));
-        sheet.getRow(6).getCell(3).setCellValue(getBeginQj().substring(5,7));
-        sheet.getRow(6).getCell(5).setCellValue(getBeginQj().substring(8,10));
-
-        sheet.getRow(7).getCell(1).setCellValue(getEndQj().substring(0,4));
-        sheet.getRow(7).getCell(3).setCellValue(getEndQj().substring(5,7));
-        sheet.getRow(7).getCell(5).setCellValue(getEndQj().substring(8,10));
-
-        //资产负债表
-        sheet = workbook.getSheetAt(1);
-        handleZcfzbSheet(sheet, zcfzTaxVoMap, zcFzBVOMap, 6, new Integer[]{2, 3, 4, 5, 6, 7}, new String[]{"qmye1", "ncye1", "qmye2", "ncye2"});
-        sheet.getRow(2).getCell(3).setCellValue(getNsrsbh());
-        sheet.getRow(2).getCell(6).setCellValue(getNsrmc());
-        sheet.getRow(3).getCell(5).setCellValue(getBeginQj());
-        sheet.getRow(3).getCell(7).setCellValue(getEndQj());
-
-        //利润表
-        sheet = workbook.getSheetAt(2);
-        handleLrbSheet(sheet, lrbTaxVoMap, lrbVOMap, 5, new Integer[]{2, 3, 4}, new String[]{"bnljje", "lastyear_bnljje"});
-        sheet.getRow(2).getCell(3).setCellValue(getNsrsbh());
-        sheet.getRow(2).getCell(6).setCellValue(getNsrmc());
-        sheet.getRow(3).getCell(3).setCellValue(getEndQj());
-        sheet.getRow(3).getCell(5).setCellValue(getBeginQj());
-        sheet.getRow(3).getCell(7).setCellValue(getEndQj());
-
-        //现金流量表
-        sheet = workbook.getSheetAt(3);
-        handleXjllSheet(sheet, xjllTaxVoMap, xjllbVOMap, 6, new Integer[]{2, 3, 4}, new String[]{"sqje", "sqje_last"});
-
-        sheet.getRow(2).getCell(3).setCellValue(getNsrsbh());
-        sheet.getRow(2).getCell(6).setCellValue(getNsrmc());
-        sheet.getRow(3).getCell(3).setCellValue(getEndQj());
-        sheet.getRow(3).getCell(5).setCellValue(getBeginQj());
-        sheet.getRow(3).getCell(7).setCellValue(getEndQj());
-        return workbook;
+//        Resource resource = ResourceUtil.get(ExportTemplateEnum.GUIZHOU, ResourceUtil.ResourceEnum.KJ2007ALL);
+//        Workbook workbook = WorkbookFactory.create(resource.getInputStream());
+//
+//        Sheet sheet = workbook.getSheetAt(0);
+//        sheet.getRow(4).getCell(1).setCellValue(getNsrsbh());
+//        sheet.getRow(5).getCell(1).setCellValue(getNsrmc());
+//
+//        sheet.getRow(6).getCell(1).setCellValue(getBeginQj().substring(0,4));
+//        sheet.getRow(6).getCell(3).setCellValue(getBeginQj().substring(5,7));
+//        sheet.getRow(6).getCell(5).setCellValue(getBeginQj().substring(8,10));
+//
+//        sheet.getRow(7).getCell(1).setCellValue(getEndQj().substring(0,4));
+//        sheet.getRow(7).getCell(3).setCellValue(getEndQj().substring(5,7));
+//        sheet.getRow(7).getCell(5).setCellValue(getEndQj().substring(8,10));
+//
+//        //资产负债表
+//        sheet = workbook.getSheetAt(1);
+//        handleZcfzbSheet(sheet, zcfzTaxVoMap, zcFzBVOMap, 6, new Integer[]{2, 3, 4, 5, 6, 7}, new String[]{"qmye1", "ncye1", "qmye2", "ncye2"});
+//        sheet.getRow(2).getCell(3).setCellValue(getNsrsbh());
+//        sheet.getRow(2).getCell(6).setCellValue(getNsrmc());
+//        sheet.getRow(3).getCell(5).setCellValue(getBeginQj());
+//        sheet.getRow(3).getCell(7).setCellValue(getEndQj());
+//
+//        //利润表
+//        sheet = workbook.getSheetAt(2);
+//        handleLrbSheet(sheet, lrbTaxVoMap, lrbVOMap, 5, new Integer[]{2, 3, 4}, new String[]{"bnljje", "lastyear_bnljje"});
+//        sheet.getRow(2).getCell(3).setCellValue(getNsrsbh());
+//        sheet.getRow(2).getCell(6).setCellValue(getNsrmc());
+//        sheet.getRow(3).getCell(3).setCellValue(getEndQj());
+//        sheet.getRow(3).getCell(5).setCellValue(getBeginQj());
+//        sheet.getRow(3).getCell(7).setCellValue(getEndQj());
+//
+//        //现金流量表
+//        sheet = workbook.getSheetAt(3);
+//        handleXjllSheet(sheet, xjllTaxVoMap, xjllbVOMap, 6, new Integer[]{2, 3, 4}, new String[]{"sqje", "sqje_last"});
+//
+//        sheet.getRow(2).getCell(3).setCellValue(getNsrsbh());
+//        sheet.getRow(2).getCell(6).setCellValue(getNsrmc());
+//        sheet.getRow(3).getCell(3).setCellValue(getEndQj());
+//        sheet.getRow(3).getCell(5).setCellValue(getBeginQj());
+//        sheet.getRow(3).getCell(7).setCellValue(getEndQj());
+//        return workbook;
+        // 默认走20196号，不走老版了
+        return createWorkBookKj2007(lrbTaxVoMap, zcfzTaxVoMap, xjllTaxVoMap, lrbVOMap, xjllbVOMap, zcFzBVOMap, "20196");
     }
 
     @Override
     public Workbook createWorkBookKj2007(Map<String, String> lrbTaxVoMap, Map<String, String> zcfzTaxVoMap, Map<String, String> xjllTaxVoMap, Map<String, LrbVO> lrbVOMap, Map<String, XjllbVO> xjllbVOMap, Map<String, ZcFzBVO> zcFzBVOMap, String versionno) throws Exception {
-        return null;
+        Resource resource = ResourceUtil.get(ExportTemplateEnum.BEIJING, ResourceUtil.ResourceEnum.KJ2007ALL,versionno);
+        Workbook workbook = WorkbookFactory.create(resource.getInputStream());
+        // 资产负债表
+        Sheet sheet = workbook.getSheetAt(1);
+        handleZcfzbSheet(sheet, zcfzTaxVoMap, zcFzBVOMap, 6, new Integer[] { 1, 3, 4, 5, 7, 8},
+                new String[] { "qmye1", "ncye1", "qmye2", "ncye2" });
+        // 利润表
+        sheet = workbook.getSheetAt(2);
+        handleLrbSheet(sheet, lrbTaxVoMap, lrbVOMap, 6, new Integer[] { 1, 2, 3 },
+                new String[] { "bnljje", "lastyear_bnljje" });
+        // 现金流量表
+        sheet = workbook.getSheetAt(3);
+        handleXjllSheet(sheet, xjllTaxVoMap, xjllbVOMap, 6, new Integer[] { 1, 2, 3 },
+                new String[] { "sqje", "sqje_last" });
+        return workbook;
     }
 
     @Override
