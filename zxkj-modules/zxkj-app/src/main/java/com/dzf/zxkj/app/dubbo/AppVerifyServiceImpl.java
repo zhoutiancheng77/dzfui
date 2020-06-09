6ed7a08fd38f06fbffee770c1da901fe4761c762
yinyixuan <329327140@qq.com>
2020-06-09 16:54:22 +0800

    短信默认改模板

--- zxkj-modules/zxkj-app/src/main/java/com/dzf/zxkj/app/dubbo/AppVerifyServiceImpl.java
+++ zxkj-modules/zxkj-app/src/main/java/com/dzf/zxkj/app/dubbo/AppVerifyServiceImpl.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @org.apache.dubbo.config.annotation.Service(version = "1.0.0", timeout = Integer.MAX_VALUE)
 public class AppVerifyServiceImpl implements IAppVerifyService {
     @Override
-    public String sendPhoneVerverify(String phone,String verify){
+    public String sendPhoneVerverify(String phone,String verify,String tempcode){
         Map<String, String> params = new HashMap<String, String>();
         params.put("verify", String.valueOf(verify));
         SMSBVO smsVO = new SMSBVO();
@@ -28,7 +28,8 @@ public class AppVerifyServiceImpl implements IAppVerifyService {
 //        }
         smsVO.setParams(params);
         smsVO.setPhone(new String[] { phone});
-        smsVO.setTemplatecode(ISmsConst.TEMPLATECODE_4101);//ISmsConst.TEMPLATECODE_4101
+       // smsVO.setTemplatecode(ISmsConst.TEMPLATECODE_4101);//ISmsConst.TEMPLATECODE_4101
+        smsVO.setTemplatecode(tempcode);
 
         //验证码:${verify}，（大账房绝对不会索取此验证码，切勿告知他人），请您10分钟内在页面中输入以完成验证。
         //【大帐房】 您正在进行短信验证码登录操作，验证码${verify}。(验证码不要告知他人，否则可能导致账号被盗，请勿泄露)
