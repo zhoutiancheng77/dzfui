8a93119465e693459b3449bbaa0ed0a6e8b47064
高志新 <gzhx>
2020-06-12 16:27:10 +0800

    去掉网关秘钥刷新

--- zxkj-gateway/src/main/java/com/dzf/zxkj/gateway/runner/AuthRunner.java
+++ zxkj-gateway/src/main/java/com/dzf/zxkj/gateway/runner/AuthRunner.java
@@ -7,7 +7,6 @@ import com.dzf.zxkj.gateway.config.GatewayConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -27,15 +26,12 @@ public class AuthRunner implements CommandLineRunner {
         try {
             refreshPubKey();
         } catch (Exception e) {
-            log.error("初始化加载pubKey失败,1分钟后自动重试!", e);
+            log.error("初始化加载pubKey失败", e);
         }
     }
-
-    @Scheduled(cron = "0 0/5 * * * ?")
     public void refreshPubKey() {
         Map<String, byte[]> keyMap = rsaKeyCache.get("rsaKey");
         byte[] pubKey = keyMap.get("pub");
         gatewayConfig.setUserPubKey(pubKey);
-        log.info("刷新公钥。。。。。。");
     }
 }
