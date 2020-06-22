ef11d8b290d0405d55f1b31662063008606cf182
yinyixuan <329327140@qq.com>
2020-06-22 11:35:45 +0800

    总账模式处理

--- zxkj-modules/zxkj-app/src/main/java/com/dzf/zxkj/app/service/app/act/impl/QryReportImpl1ServiceImpl.java
+++ zxkj-modules/zxkj-app/src/main/java/com/dzf/zxkj/app/service/app/act/impl/QryReportImpl1ServiceImpl.java
@@ -384,9 +384,15 @@ public class QryReportImpl1ServiceImpl implements IQryReport1Service {
 						appfzmxvo.setYe(Common.format(qmye));
 						appfzmxvo.setBqfsjf(Common.format(fzyevo.getBqfsjf()));
 						appfzmxvo.setBqfsdf(Common.format(fzyevo.getBqfsdf()));
+						appfzmxvo.setFx(fzyevo.getAccDirection()== 1 ? "贷":"借"); //0借1贷
 						mxlist.add(appfzmxvo);
 						yevo.setMxvos(mxlist.toArray(new AppFzmxVo[0]));
-						yetotal = SafeCompute.add(yetotal, qmye);
+						if(fzyevo.getAccDirection() == 1){
+							yetotal = SafeCompute.add(yetotal, qmye);
+						}else{
+							yetotal = SafeCompute.sub(yetotal, qmye);
+						}
+
 					}
 				}
 			}
