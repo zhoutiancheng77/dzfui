0780199d163d230b9010a80cbdf9cf75f6ce1865
zhangj <zhangj@admin-PC>
2020-06-30 16:58:46 +0800

    11

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/service/zcgl/impl/AssetCardImpl.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/service/zcgl/impl/AssetCardImpl.java
@@ -1,5 +1,6 @@
 package com.dzf.zxkj.platform.service.zcgl.impl;
 
+import cn.jiguang.common.utils.StringUtils;
 import com.dzf.zxkj.base.dao.SingleObjectBO;
 import com.dzf.zxkj.base.framework.SQLParameter;
 import com.dzf.zxkj.base.framework.processor.ArrayProcessor;
@@ -648,6 +649,7 @@ public class AssetCardImpl implements IAssetCard {
         }
 
         Map<String, YntCpaccountVO> cpamap = accountService.queryMapByPk(corpvo.getPk_corp());
+        StringBuffer zjfykm_key = new StringBuffer();
         for (AssetcardVO assetcardVO : assetcardVOList) {
             // 生成凭证, 查询资产类型编码
             BdAssetCategoryVO bdAssetCategoryVO = categoryVoMap.get(assetcardVO.getAssetcategory());
@@ -665,8 +667,16 @@ public class AssetCardImpl implements IAssetCard {
                         || cpamap.get(assetcardVO.getPk_zjfykm()) == null) {
                     throw new BusinessException("折旧科目不存在!");
                 }
+                zjfykm_key = new StringBuffer();
+                zjfykm_key.append(assetcardVO.getPk_zjfykm());
+                for (int i =1 ;i<=10;i++) {
+                    if (assetcardVO.getAttributeValue("zjfyfzhsx"+i) != null
+                    && !StringUtils.isEmpty((String)assetcardVO.getAttributeValue("zjfyfzhsx"+i))) {
+                        zjfykm_key.append(","+assetcardVO.getAttributeValue("zjfyfzhsx"+i));
+                    }
+                }
                 // 借方
-                if (keymap.containsKey((0 + "_" + assetcardVO.getPk_zjfykm()))) {
+                if (keymap.containsKey((0 + "_" + zjfykm_key.toString()))) {
                     for (TzpzBVO bbvo : tzpzBVoList) {
                         String dir;
                         if (bbvo.getJfmny() != null
@@ -706,7 +716,7 @@ public class AssetCardImpl implements IAssetCard {
                     creditVO.setDr(0);
                     creditVO.setRowno(2 * rowno + 1);
                     creditVO.setVdirect(0);
-                    keymap.put(0 + "_" + assetcardVO.getPk_zjfykm(), creditVO);
+                    keymap.put(0 + "_" + zjfykm_key.toString(), creditVO);
                     tzpzBVoList.add(creditVO);
                 }
 
