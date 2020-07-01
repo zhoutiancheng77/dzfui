78ba07fb5827cdc6e60ce2eb8845936de50a5363
wangzhaoning <wangzhaoning@dazhangfang.com>
2020-07-01 14:54:01 +0800

    增加rcunitname

--- zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/util/CryptCodeUtil.java
+++ zxkj-modules/zxkj-platform/src/main/java/com/dzf/zxkj/platform/util/CryptCodeUtil.java
@@ -2,15 +2,8 @@ package com.dzf.zxkj.platform.util;
 
 
 import com.dzf.zxkj.base.exception.DZFWarpException;
-import com.dzf.zxkj.base.exception.WiseRunException;
-import com.dzf.zxkj.common.utils.Base64CodeUtils;
 import com.dzf.zxkj.common.utils.StringUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
 
 /**
  * 数据加密解密
@@ -18,36 +11,36 @@ import java.io.ObjectInputStream;
 @Slf4j
 public class CryptCodeUtil {
 	
-	private static ClassPathResource resource = new ClassPathResource("encrypt_key.txt");
+//	private static ClassPathResource resource = new ClassPathResource("encrypt_key.txt");
 
-	private static String pubkey;
+//	private static String pubkey = "d3d3LmRhemhhbmdmYW5nLmNvbQ==";
+
+	private static String defaultkey = "www.dazhangfang.com";
 	
-	private static String defaultkey;
-	
-	private static void readUIParameter(){
-		FileInputStream fis = null;
-		ObjectInputStream ois = null;
-		try {
-			String path = resource.getURL().getPath(); 
-			File f = new File(path);
-			if (f.isFile()){
-				fis = new FileInputStream(f);
-				ois = new ObjectInputStream(fis);
-				pubkey = (String)ois.readObject();
-				defaultkey = new String(Base64CodeUtils.decode(pubkey));
-			}
-		}catch (Exception ex){
-			log.error("错误",ex);
-			throw new WiseRunException(ex);
-		}
-	}
+//	private static void readUIParameter(){
+//		FileInputStream fis = null;
+//		ObjectInputStream ois = null;
+//		try {
+//			String path = resource.getURL().getPath();
+//			File f = new File(path);
+//			if (f.isFile()){
+//				fis = new FileInputStream(f);
+//				ois = new ObjectInputStream(fis);
+//				pubkey = (String)ois.readObject();
+//				defaultkey = new String(Base64CodeUtils.decode(pubkey));
+//			}
+//		}catch (Exception ex){
+//			log.error("错误",ex);
+//			throw new WiseRunException(ex);
+//		}
+//	}
 
 	//这里异常吃掉
 	public static String enCode(String value) throws DZFWarpException {
 		if(StringUtil.isEmpty(value))
 			return value;
-		if(pubkey == null || "".equals(defaultkey))
-			readUIParameter();
+//		if(pubkey == null || "".equals(defaultkey))
+//			readUIParameter();
 		String key = null;
 		try{
 			key =  RC4.encrypt(value, defaultkey);
@@ -62,8 +55,8 @@ public class CryptCodeUtil {
 	public static String deCode(String pvalue) throws DZFWarpException{
 		if(StringUtil.isEmpty(pvalue))
 			return pvalue;
-		if(pubkey == null || "".equals(defaultkey))
-			readUIParameter();
+//		if(pubkey == null || "".equals(defaultkey))
+//			readUIParameter();
 		String key = null;
 		try{
 			key = RC4.decrypt(pvalue,defaultkey);
