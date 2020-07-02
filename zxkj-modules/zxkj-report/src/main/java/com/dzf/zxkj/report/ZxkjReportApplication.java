e47dedc0632f2d8fe10212f67bec14b1797a1cb6
高志新 <gzhx>
2020-07-02 15:30:17 +0800

    查询主备库

--- zxkj-modules/zxkj-report/src/main/java/com/dzf/zxkj/report/ZxkjReportApplication.java
+++ zxkj-modules/zxkj-report/src/main/java/com/dzf/zxkj/report/ZxkjReportApplication.java
@@ -1,5 +1,6 @@
 package com.dzf.zxkj.report;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,9 +11,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @Date: 2019-09-05
  * @Description:
  */
-@SpringBootApplication
 @EnableDiscoveryClient
 @EnableScheduling
+@SpringBootApplication(exclude = DruidDataSourceAutoConfigure.class)
 public class ZxkjReportApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZxkjReportApplication.class, args);
